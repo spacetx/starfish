20bd3d96fbfffc1810d57ba25f2dc2a7f558d4f3
Deep Ganguli <deep.ganguli@gmail.com>
2017-06-30 09:45:35 -0700

    stats: added regional stats

--- starfish/stats.py
+++ starfish/stats.py
@@ -1,5 +1,16 @@
 import numpy as np
 import pandas as pd
+from scipy.sparse import coo_matrix
+from scipy.ndimage import morphology
+from skimage.morphology import erosion, dilation, opening
+from regional import one as One
+from regional import many as Many
+
+
+def stack_stat(stack):
+    num_hybs = stack.shape[0]
+    stats = [im_stat(stack[k, :]) for k in range(num_hybs)]
+    return stats
 
 
 def im_stat(im):
@@ -10,7 +21,16 @@ def im_stat(im):
     return stats.to_dict()
 
 
-def stack_stat(stack):
-    num_hybs = stack.shape[0]
-    stats = [im_stat(stack[k, :]) for k in range(num_hybs)]
-    return stats
+def regions_stat(label_mat):
+    coo = coo_matrix(label_mat)
+    regions = [region_for(cell, coo) for cell in set(coo.data)]
+    return Many(regions)
+
+
+def region_for(label_mat_coo, label):
+    ind = label_mat_coo.data == label
+    #TODO does this work in 3D?
+    x = label_mat_coo.row[ind]
+    y = label_mat_coo.col[ind]
+    re = One(zip(x, y))
+    return re

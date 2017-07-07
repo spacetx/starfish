077497985e958a93185a0c17f1c0a5e6651b099a
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-07 15:23:28 -0700

    stats: add image measurements

--- starfish/stats.py
+++ starfish/stats.py
@@ -22,16 +22,28 @@ def im_describe(im):
     return stats.to_dict()
 
 
-def regions_stat(label_mat):
-    label_mat_coo = coo_matrix(label_mat)
+def label_to_regions(labels):
+    label_mat_coo = coo_matrix(labels)
+
+    def region_for(label_mat_coo, label):
+        ind = label_mat_coo.data == label
+        # TODO does this work in 3D?
+        x = label_mat_coo.row[ind]
+        y = label_mat_coo.col[ind]
+        re = One(zip(x, y))
+        return re
+
     regions = [region_for(label_mat_coo, label) for label in set(label_mat_coo.data)]
+
     return Many(regions)
 
 
-def region_for(label_mat_coo, label):
-    ind = label_mat_coo.data == label
-    #TODO does this work in 3D?
-    x = label_mat_coo.row[ind]
-    y = label_mat_coo.col[ind]
-    re = One(zip(x, y))
-    return re
+def measure_mean(im, labels, num_objs):
+    means = spm.mean(im, labels, range(0, num_objs))
+    return means
+
+
+def measure_mean_stack(stack, labels, num_objs):
+    ims = stack_to_list(stack)
+    res = [measure_mean(im, labels, num_objs) for im in ims]
+    return res

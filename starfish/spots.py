231334ad6ceb9707c4e6e346bf1452b164107afd
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-11 14:18:56 -0700

    simple spot detector: refactor

--- starfish/spots.py
+++ starfish/spots.py
@@ -1,6 +1,8 @@
-import scipy.ndimage.measurements as spm
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.ndimage.measurements as spm
+from showit import image
 
 from starfish.filters import bin_thresh
 from starfish.munge import scale, max_proj, gather
@@ -12,27 +14,66 @@ class SimpleSpotDetector:
         self.stack = stack
         self.thresh = thresh
 
-    def go(self):
+        self.mp_thresh = None
+        self.labels = None
+        self.num_objs = None
+        self.areas = None
+        self.intensities = None
+
+        self.spots_df = None
+
+    def detect(self):
+        self.mp_thresh = self._threshold()
+        self.labels, self.num_objs = self._label()
+        self.areas, self.intensities = self._measure()
+
+    def _threshold(self):
         mp = scale(self.stack, 'max')
         mp = max_proj(mp)
-        mp_thresh = bin_thresh(mp,self.thresh)
-        labels, num_objs = spm.label(mp_thresh)
+        mp_thresh = bin_thresh(mp, self.thresh)
+        return mp_thresh
 
-        self.areas = spm.sum(np.ones(labels.shape),
-                             labels,
-                             np.array(range(0, num_objs + 1), dtype=np.int32))
+    def _label(self):
+        labels, num_objs = spm.label(self.mp_thresh)
+        return labels, num_objs
 
-        e = self.areas[(self.areas > 1) & (self.areas <= 10)]
+    def _measure(self):
+        areas = spm.sum(np.ones(self.labels.shape),
+                        self.labels,
+                        np.array(range(0, self.num_objs + 1), dtype=np.int32))
 
-        self.regions = label_to_regions(labels)
+        intensity = measure_mean_stack(self.stack, self.labels, self.num_objs)
+        return areas, intensity
 
-        res = measure_mean_stack(self.stack, labels, num_objs)
+    def to_regions(self):
+        regions = label_to_regions(self.labels)
+        return regions
 
+    def to_dataframe(self, tidy_flag):
         num_hybs = self.stack.shape[0]
         cols = range(num_hybs)
         cols = ['hyb_{}'.format(c + 1) for c in cols]
-        d = dict(zip(cols, res))
-        d['spot_id'] = range(num_objs)
+        d = dict(zip(cols, self.intensities))
+        d['spot_id'] = range(self.num_objs)
 
-        t = pd.DataFrame(d)
-        f = gather(t, 'hybs', 'vals', cols)
+        res = pd.DataFrame(d)
+
+        if tidy_flag:
+            res = gather(res, 'hybs', 'vals', cols)
+
+        return res
+
+    def show(self, figsize=(10, 10)):
+        plt.figure(figsize=figsize)
+        plt.subplot(211)
+        image(self.mp_thresh, bar=True, size=10, ax=plt.gca())
+
+        plt.subplot(212)
+        regions = self.to_regions()
+        image(regions.mask(background=[0.9, 0.9, 0.9],
+                           dims=self.labels.shape,
+                           stroke=None,
+                           cmap='rainbow'),
+              size=10,
+              ax=plt.gca()
+              )

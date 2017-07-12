8f8a50aeb3ae27868e42108e6693da5fbff7f947
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-12 14:08:29 -0700

    assign: initial comment. assigns spots to cells

---
+++ starfish/assign.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from skimage.measure import points_in_poly
+
+from starfish.stats import label_to_regions
+
+
+def assign(cells_label, spots_label, verbose=False):
+    cells_region = label_to_regions(cells_label)
+    spots_region = label_to_regions(spots_label)
+
+    res = pd.DataFrame({'spot_id': range(0, spots_region.count)})
+    res['cell_id'] = None
+
+    points = spots_region.center
+    points = np.array(points)
+
+    for cell_id in range(cells_region.count):
+        verts = cells_region[cell_id].coordinates
+        verts = np.array(verts)
+        in_poly = points_in_poly(points, verts)
+        res.loc[res.spot_id[in_poly], 'cell_id'] = cell_id
+        if verbose:
+            cnt = np.sum(in_poly)
+            print cell_id, cnt
+
+    return res
+
+

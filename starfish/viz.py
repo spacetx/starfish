556e54df3fa2dfb6ccb8ba98428cbdcaa3c6e504
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-12 14:28:22 -0700

    viz: add image lims

--- starfish/viz.py
+++ starfish/viz.py
@@ -1,7 +1,15 @@
-from showit import tile
+from showit import image, tile
+
 
 def tile_lims(stack, num_std, bar=True, size=20):
     from starfish.stats import stack_describe
     stats = stack_describe(stack)
     lims = [s['mean'] + num_std * s['std'] for s in stats]
     tile(stack, bar=bar, clim=zip([0] * len(lims), lims), size=size)
+
+
+def image_lims(im, num_std, bar=True, size=20):
+    from starfish.stats import im_describe
+    stats = im_describe(im)
+    lim = stats['mean'] + num_std * stats['std']
+    image(im, bar=bar, size=size, clim=[0, lim])

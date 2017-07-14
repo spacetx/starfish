4275a57e1a156e9cb0e85724a2e4b3c365faa0a3
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-14 16:57:25 -0700

    assign: use convex hulls if desired

--- starfish/assign.py
+++ starfish/assign.py
@@ -5,7 +5,7 @@ from skimage.measure import points_in_poly
 from starfish.stats import label_to_regions
 
 
-def assign(cells_label, spots_label, verbose=False):
+def assign(cells_label, spots_label, use_hull=True, verbose=False):
     cells_region = label_to_regions(cells_label)
     spots_region = label_to_regions(spots_label)
 
@@ -16,7 +16,10 @@ def assign(cells_label, spots_label, verbose=False):
     points = np.array(points)
 
     for cell_id in range(cells_region.count):
-        verts = cells_region[cell_id].coordinates
+        if use_hull:
+            verts = cells_region[cell_id].hull
+        else:
+            verts = cells_region[cell_id].coordinates
         verts = np.array(verts)
         in_poly = points_in_poly(points, verts)
         res.loc[res.spot_id[in_poly], 'cell_id'] = cell_id
@@ -25,5 +28,3 @@ def assign(cells_label, spots_label, verbose=False):
             print cell_id, cnt
 
     return res
-
-

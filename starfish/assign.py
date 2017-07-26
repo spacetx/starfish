de97a081ab9396b34fa7cbd9cae1998b9e2ecf1c
Deep Ganguli <deep.ganguli@gmail.com>
2017-07-26 15:59:42 -0700

    assign: spots are ndarray instead of regional

--- starfish/assign.py
+++ starfish/assign.py
@@ -5,15 +5,11 @@ from skimage.measure import points_in_poly
 from starfish.stats import label_to_regions
 
 
-def assign(cells_label, spots_label, use_hull=True, verbose=False):
+def assign(cells_label, spots, use_hull=True, verbose=False):
     cells_region = label_to_regions(cells_label)
-    spots_region = label_to_regions(spots_label)
 
-    res = pd.DataFrame({'spot_id': range(0, spots_region.count)})
+    res = pd.DataFrame({'spot_id': range(0, spots.shape[0])})
     res['cell_id'] = None
-
-    points = spots_region.center
-    points = np.array(points)
 
     for cell_id in range(cells_region.count):
         if use_hull:
@@ -21,7 +17,7 @@ def assign(cells_label, spots_label, use_hull=True, verbose=False):
         else:
             verts = cells_region[cell_id].coordinates
         verts = np.array(verts)
-        in_poly = points_in_poly(points, verts)
+        in_poly = points_in_poly(spots, verts)
         res.loc[res.spot_id[in_poly], 'cell_id'] = cell_id
         if verbose:
             cnt = np.sum(in_poly)

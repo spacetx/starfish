4c118b741183053a7bfd16455bbcd6ce92410db9
Deep Ganguli <deep.ganguli@gmail.com>
2017-08-19 12:58:12 -0700

    io: more sane read/write api and some convenient reshape methods

--- starfish/io.py
+++ starfish/io.py
@@ -4,65 +4,84 @@ import numpy as np
 
 
 class Stack:
-    def __init__(self, fov_path, aux_path):
-        self.fov_path = fov_path
-        self.aux_path = aux_path
+    def __init__(self):
 
-        self.fov_df = None
-        self.aux_df = None
+        # data organization, dataframes
+        self.org = None
+        self.aux_org = None
 
-        self.stack = None
+        # numpy array (num_hybs, num_chans, x, y, z)
+        self.data = None
+
+        # shape of array
         self.num_hybs = None
         self.num_chans = None
-        self.dims = None
         self.im_shape = None
         self.is_volume = None
 
+        # auxilary images
         self.dapi = None
         self.aux_dict = dict()
 
-    def read(self):
-        self._read_fov()
-        self._read_aux()
+    def read(self, fov_path, aux_path):
+        self._read_fov(fov_path)
+        if aux_path is not None:
+            self._read_aux(aux_path)
 
-    def _read_fov(self):
-        self.fov_df = pd.read_csv(self.fov_path)
-        self.num_hybs = self.fov_df.hyb.max()
-        self.num_chans = self.fov_df.ch.max()
+    def write(self, dir_name, ext):
+
+        hybs = self.org.hyb.values
+        chs = self.org.ch.values
+        inds = zip(hybs, chs)
+
+        fnames = []
+        for h, c in inds:
+            im = self.data[h, c, :]
+            fname = dir_name + '/h{}_c{}.{}'.format(h, c, ext)
+            io.imsave(fname, im)
+            fnames.append(fname)
+
+        org = pd.DataFrame({'file': fnames, 'hyb': hybs, 'ch': chs})
+        org.to_csv(dir_name + '/org.csv', index=False)
+
+    def _read_fov(self, fov_path):
+        self.org = pd.read_csv(fov_path)
+        self.num_hybs = self.org.hyb.max()
+        self.num_chans = self.org.ch.max()
 
         # correct for off by one error
-        if self.fov_df.hyb.min() == 0:
+        if self.org.hyb.min() == 0:
             self.num_hybs += 1
-        elif self.fov_df.hyb.min() == 1:
-            self.fov_df.hyb -= 1
+        elif self.org.hyb.min() == 1:
+            self.org.hyb -= 1
 
-        if self.fov_df.ch.min() == 0:
+        if self.org.ch.min() == 0:
             self.num_chans += 1
-        elif self.fov_df.ch.min() == 1:
-            self.fov_df.ch -= 1
+        elif self.org.ch.min() == 1:
+            self.org.ch -= 1
 
         # determine image shape, set volumetric flag
-        im = io.imread(self.fov_df.file[0])
+        im = io.imread(self.org.file[0])
         self.im_shape = im.shape
 
         if len(self.im_shape) == 2:
-            self.stack = np.zeros((self.num_hybs, self.num_chans, self.im_shape[0], self.im_shape[1]))
+            self.data = np.zeros((self.num_hybs, self.num_chans, self.im_shape[0], self.im_shape[1]))
             self.is_volume = False
         else:
-            self.stack = np.zeros((self.num_hybs, self.num_chans, self.im_shape[0], self.im_shape[1], self.im_shape[2]))
+            self.data = np.zeros((self.num_hybs, self.num_chans, self.im_shape[0], self.im_shape[1], self.im_shape[2]))
             self.is_volume = True
 
-        org = zip(self.fov_df.hyb.values, self.fov_df.ch.values, self.fov_df.file)
+        org = zip(self.org.hyb.values, self.org.ch.values, self.org.file)
 
         for h, c, fname in org:
-            self.stack[h, c, :] = io.imread(fname)
+            self.data[h, c, :] = io.imread(fname)
 
-    def _read_aux(self):
-        self.aux_df = pd.read_csv(self.aux_path)
-        dapi_path = self.aux_df[self.aux_df.type == 'dapi'].file.values[0]
+    def _read_aux(self, aux_path):
+        self.aux_org = pd.read_csv(aux_path)
+        dapi_path = self.aux_org[self.aux_org.type == 'dapi'].file.values[0]
         self.dapi = io.imread(dapi_path)
 
-        org = zip(self.aux_df.type.values, self.aux_df.file.values)
+        org = zip(self.aux_org.type.values, self.aux_org.file.values)
 
         for typ, fname in org:
             if typ == 'dapi':
@@ -70,15 +89,15 @@ class Stack:
             else:
                 self.aux_dict[typ] = io.imread(fname)
 
-    def write(self, fov_path, aux_path):
-        self._write_fov(fov_path)
-        self._write_aux(aux_path)
+    @property
+    def shape(self):
+        return self.data.shape
 
-    def _write_fov(self, fov_path):
-        pass
-
-    def _write_aux(self, aux_path):
-        pass
+    def set_stack(self, new_stack):
+        if new_stack.shape != self.shape:
+            msg = "Shape mismatch. Current data shape: {}, new data shape: {}".formmat(self.shape, new_stack.shape)
+            raise AttributeError(msg)
+        self.data = new_stack
 
     def max_proj(self, dim):
         valid_dims = ['hyb', 'ch', 'z']
@@ -87,15 +106,29 @@ class Stack:
             raise ValueError(msg)
 
         if dim == 'hyb':
-            res = np.max(self.stack, axis=0)
+            res = np.max(self.data, axis=0)
         elif dim == 'ch':
-            res = np.max(self.stack, axis=1)
+            res = np.max(self.data, axis=1)
         elif dim == 'z' and self.is_volume:
-            res = np.max(self.stack, axis=4)
+            res = np.max(self.data, axis=4)
         else:
-            res = self.stack
+            res = self.data
 
         return res
 
-    def combine_hyb_ch(self):
+    def squeeze(self):
+        new_shape = ((self.num_hybs * self.num_chans),) + self.im_shape
+        new_data = np.zeros(new_shape)
+
+        # TODO this can probably done smartly with np.reshape instead of a double for loop
+        ind = 0
+        for h in range(self.num_hybs):
+            for c in range(self.num_chans):
+                new_data[ind, :] = self.data[h, c, :]
+                ind += 1
+
+        return new_data
+
+    def un_squeeze(self, stack):
+        new_shape = (self.num_hybs, self.num_chans) + self.im_shape
         pass

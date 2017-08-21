139fd86c3dbf4e3064819ca2df1514d6efcd7cec
Deep Ganguli <deep.ganguli@gmail.com>
2017-08-20 21:33:27 -0700

    viz: added spots and regions to geo_json code

--- starfish/viz.py
+++ starfish/viz.py
@@ -1,4 +1,9 @@
+import json
+
+import scipy.misc
 from showit import image, tile
+
+from starfish.stats import label_to_regions
 
 
 def tile_lims(stack, num_std, bar=True, size=20):
@@ -13,3 +18,36 @@ def image_lims(im, num_std, bar=True, size=20):
     stats = im_describe(im)
     lim = stats['mean'] + num_std * stats['std']
     image(im, bar=bar, size=size, clim=[0, lim])
+
+
+def spots_geo_json(spots_viz_df, fname):
+    def make_spot_dict(row):
+        row = row[1]
+        d = dict()
+        d['properties'] = {'id': int(row.spot_id), 'radius': int(row.r)}
+        d['geometry'] = {'type': 'Point', 'coordinates': [int(row.x), int(row.y)]}
+        return d
+
+    spots_json = [make_spot_dict(row) for row in spots_viz_df.iterrows()]
+
+    with open(fname, 'wb') as outfile:
+        json.dump(spots_json, outfile)
+
+    return spots_json
+
+
+def regions_geo_json(region_labels, fname):
+    r = label_to_regions(region_labels)
+
+    def make_region_dict(id, verts):
+        d = dict()
+        d["properties"] = {"id": id}
+        d["geometry"] = {"type": "Polygon",
+                         "coordinates": list(map(lambda x: list(x), verts.astype(int)))
+                         }
+        return d
+
+    regions_json = [make_region_dict(id, verts) for id, verts in enumerate(r.hull)]
+
+    with open(fname, 'wb') as outfile:
+        json.dump(regions_json, outfile)

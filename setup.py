d72612d1a1fe6ae6da76cedcede7ef7a504a69e7
Deep Ganguli <deep.ganguli@gmail.com>
2017-08-22 13:02:05 -0700

    setup.py: edit description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="starfish",
     version="0.0.1",
-    description="prototype a standardized pipeline for the analysis of image-based transcriptomics data",
+    description="Pipelines and pipeline components for the analysis of image-based transcriptomics data",
     author="Deep Ganguli",
     author_email="dganguli@chanzuckerberg.com",
     license="MIT",

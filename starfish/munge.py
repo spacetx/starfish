71ad6c6fb81c29a1cb87bbce4709e48522002192
Deep Ganguli <deep.ganguli@gmail.com>
2017-08-24 00:27:32 -0700

    whitespace ftw

--- starfish/munge.py
+++ starfish/munge.py
@@ -30,11 +30,11 @@ def scale(stack, metric, clip=False):
 
 
 def gather(df, key, value, cols):
-    id_vars = [ col for col in df.columns if col not in cols ]
+    id_vars = [col for col in df.columns if col not in cols]
     id_values = cols
     var_name = key
     value_name = value
-    return pd.melt( df, id_vars, id_values, var_name, value_name )
+    return pd.melt(df, id_vars, id_values, var_name, value_name)
 
 
 def relabel(image):

0eb847491965f68c41b1dbe2da983e63e0b0dfc2
freeman-lab <the.freeman.lab@gmail.com>
2017-08-24 20:25:54 -0700

    use regl for visualization example

---
+++ viz/camera.js
@@ -0,0 +1,64 @@
+var createCamera = require('orbit-camera')
+var createScroll = require('scroll-speed')
+var mp = require('mouse-position')
+var mb = require('mouse-pressed')
+var key = require('key-pressed')
+
+var panSpeed = 1
+
+module.exports = attachCamera
+
+function attachCamera(canvas, opts) {
+  opts = opts || {}
+  opts.pan = opts.pan !== false
+  opts.scale = opts.scale !== false
+  opts.rotate = opts.rotate !== false
+
+  var scroll = createScroll(canvas, opts.scale)
+  var mbut = mb(canvas, opts.rotate)
+  var mpos = mp(canvas)
+  var camera = createCamera(
+      [0, 0, 1]
+    , [0, 0, 0]
+    , [0, 1, 1]
+  )
+
+  camera.tick = tick
+
+  return camera
+
+  function tick() {
+    var ctrl = key('<control>') || key('<alt>')
+    var alt = key('<shift>')
+    var height = canvas.height
+    var width = canvas.width
+
+    if (opts.rotate && mbut.left && ctrl && !alt) {
+      camera.rotate(
+          [ mpos.x / width - 0.5, mpos.y / height - 0.5 ]
+        , [ mpos.prevX / width - 0.5, mpos.prevY / height - 0.5 ]
+      )
+    }
+
+    if (opts.pan && mbut.right || (mbut.left && !ctrl && !alt)) {
+      camera.pan([
+          panSpeed * (mpos.x - mpos.prevX) / width
+        , panSpeed * (mpos.y - mpos.prevY) / height
+      ])
+    }
+
+    if (opts.scale && scroll[1]) {
+      camera.distance *= Math.exp(scroll[1] / height)
+    }
+
+    if (opts.scale && (mbut.middle || (mbut.left && !ctrl && alt))) {
+      var d = mpos.y - mpos.prevY
+      if (!d) return;
+
+      camera.distance *= Math.exp(d / height)
+    }
+
+    scroll.flush()
+    mpos.flush()
+  }
+}
--- viz/index.js
+++ viz/index.js
@@ -1,83 +1,148 @@
+const resl = require('resl')
 const css = require('dom-css')
-const panzoom = require('pan-zoom')
-const rune = require('rune.js')
+const mat4 = require('gl-mat4')
+const fit = require('canvas-fit')
+const control = require('control-panel')
 
-const regions = require('./example/regions.json')
-const spots = require('./example/spots.json')
+// setup canvas and camera
+const canvas = document.body.appendChild(document.createElement('canvas'))
+const regl = require('regl')(canvas)
+const camera = require('./camera.js')(canvas, {scale: false, rotate: true})
+window.addEventListener('resize', fit(canvas), false)
 
-var scale = 0.7
+// load spots
+var spots = require('./example/spots.json')
+spots = spots.map(function (spot) {
+	var xy = spot.geometry.coordinates
+	return [xy[1] / 665 - 1.0, xy[0] / 490 - 1]
+})
 
-var width, height
-var img = new Image()
-img.src = '../example/background.png'
-img.addEventListener('load', function(e) {
-	width = img.width * scale
-	height = img.height * scale
-  	setup()
-}, false)
-
-function setup() {
-	var container = document.createElement('div')
-	document.body.append(container)
-	css(container, {
-		width: width,
-		height: height,
-		position: 'absolute'
-	})
-
-	container.append(img)
-	css(img, {
-		width: width,
-		height: height,
-		position: 'absolute',
-		'z-index': -1000
-	})
-
-	var svg = document.createElement('svg')
-	container.append(svg)
-
-	// panzoom(img, function (e) {
-	// 	css(img, {'transform': 'scale(' + e.dz + ')'})
-	// 	console.log('translate(' + e.x + 'px, ' + e.y + 'px) scale(' + e.dz + ')')
-	// })
-
-	console.log(spots)
-	console.log(regions)
-
-	var r = new rune({
-	  container: container,
-	  width: width,
-	  height: height
-	});
-
-	var group = r.group(0, 0);
-
-	var xy, rad
-	spots.forEach(function (spot) {
-		xy = spot.geometry.coordinates
-		rad = spot.properties.radius
-		r.circle(xy[1] * scale, xy[0] * scale, rad, group)
-			.fill(false)
-			.stroke(150,150,150)
-	})
-
-	var verts, start
-	regions.forEach(function (region) {
-		verts = region.geometry.coordinates
-		console.log(verts)
-		start = r.polygon(0, 0)
-			.stroke(150,150,150)
-  			.fill(false)
-		verts.forEach(function (v) {
-			start.lineTo(v[1] * scale, v[0] * scale)
-		})
-	})
-
-	r.draw()
+// setup control panel and state
+var state = {
+	show:  true,
+	color: [0.6, 0.2, 0.9]
 }
+var panel = control([
+	{type: 'color', label: 'dot color', format: 'array', initial: state.color},
+	{type: 'checkbox', label: 'show dots', initial: state.show}
+],
+	{theme: 'dark', position: 'top-left'}
+)
+panel.on('input', function (data) {
+	state.color = typeof(data['dot color']) == 'string' ? state.color : data['dot color']
+	state.show = data['show dots']
+})
 
-// get pan and zoom working
+// create regl spot drawing function
+const drawSpots = regl({
+  vert: `
+  precision mediump float;
+  attribute vec2 position;
+  uniform float distance;
+  uniform vec3 color;
+  uniform mat4 projection, view;
+  varying vec3 fragColor;
+  void main() {
+    gl_PointSize = 6.0 / pow(distance, 0.5);
+    gl_Position = view * vec4(position.x, -position.y, 0, 1);
+    fragColor = color;
+  }`,
 
-// render points with pan and zoom
+  frag: `
+  precision lowp float;
+  varying vec3 fragColor;
+  void main() {
+    if (length(gl_PointCoord.xy - 0.5) > 0.5) {
+      discard;
+    }
+    gl_FragColor = vec4(fragColor, 1);
+  }`,
 
-// render polygons with pan and zoom
+  attributes: {
+    position: spots
+  },
+
+  uniforms: {
+  	color: regl.prop('color'),
+  	distance: regl.prop('distance'),
+    view: () => camera.view(),
+    projection: ({viewportWidth, viewportHeight}) =>
+      mat4.perspective([],
+        Math.PI / 2,
+        viewportWidth / viewportHeight,
+        0.01,
+        1000),
+  },
+
+  count: spots.length,
+
+  primitive: 'points'
+})
+
+// create regl function for drawing background
+var drawBackground = regl({
+  frag: `
+  precision mediump float;
+  uniform sampler2D texture;
+  varying vec2 uv;
+  void main () {
+    gl_FragColor = texture2D(texture, uv);
+  }`,
+
+  vert: `
+  precision mediump float;
+  uniform mat4 projection, view;
+  attribute vec2 position;
+  varying vec2 uv;
+  void main () {
+    uv = position;
+    gl_Position = view * vec4(-(1.0 - 2.0 * position.x), 1.0 - 2.0 * position.y, 0, 1);
+  }`,
+
+  attributes: {
+    position: [
+      -2, 0,
+      0, -2,
+      2, 2]
+  },
+
+  uniforms: {
+    texture: regl.prop('background'),
+    view: () => camera.view(),
+    projection: ({viewportWidth, viewportHeight}) =>
+      mat4.perspective([],
+        Math.PI / 2,
+        viewportWidth / viewportHeight,
+        0.01,
+        1000),
+  },
+
+  count: 3
+})
+
+// load background and draw
+resl({
+  manifest: {
+    'background': {
+      type: 'image',
+      src: '../example/background.png'
+    },
+  },
+
+  onDone: ({background}) => {
+    const texture = regl.texture(background)
+
+    regl.frame(() => {
+    	regl.clear({
+		    depth: 1,
+		    color: [0, 0, 0, 1]
+		})
+		if (state.show) {
+			drawSpots({distance: camera.distance, color: state.color})
+		}
+    	drawBackground({background: texture})
+    	camera.tick()
+    	console.log(camera.view())
+    })
+  }
+})

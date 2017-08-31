f3acd0368b1101a81fc169a85d10399a664c1b7b
Deep Ganguli <deep.ganguli@gmail.com>
2017-08-31 14:32:02 -0700

    remove geojson from viz

--- starfish/viz.py
+++ starfish/viz.py
@@ -19,35 +19,3 @@ def image_lims(im, num_std, bar=True, size=20):
     lim = stats['mean'] + num_std * stats['std']
     image(im, bar=bar, size=size, clim=[0, lim])
 
-
-def spots_geo_json(spots_viz_df, fname):
-    def make_spot_dict(row):
-        row = row[1]
-        d = dict()
-        d['properties'] = {'id': int(row.spot_id), 'radius': int(row.r)}
-        d['geometry'] = {'type': 'Point', 'coordinates': [int(row.x), int(row.y)]}
-        return d
-
-    spots_json = [make_spot_dict(row) for row in spots_viz_df.iterrows()]
-
-    with open(fname, 'wb') as outfile:
-        json.dump(spots_json, outfile)
-
-    return spots_json
-
-
-def regions_geo_json(region_labels, fname):
-    r = label_to_regions(region_labels)
-
-    def make_region_dict(id, verts):
-        d = dict()
-        d["properties"] = {"id": id}
-        d["geometry"] = {"type": "Polygon",
-                         "coordinates": list(map(lambda x: list(x), verts.astype(int)))
-                         }
-        return d
-
-    regions_json = [make_region_dict(id, verts) for id, verts in enumerate(r.hull)]
-
-    with open(fname, 'wb') as outfile:
-        json.dump(regions_json, outfile)

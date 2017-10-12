3b360c203572c9c94358d936896c1d6fd17ab4d3
Deep Ganguli <deep.ganguli@gmail.com>
2017-10-12 11:23:44 -0700

    bug fix to richardson lucy deconv

--- starfish/viz.py
+++ starfish/viz.py
@@ -1,21 +1,18 @@
-import json
-
-import scipy.misc
+import numpy as np
 from showit import image, tile
 
-from starfish.stats import label_to_regions
+from .stats import im_describe, stack_describe
 
 
 def tile_lims(stack, num_std, bar=True, size=20):
-    from starfish.stats import stack_describe
+    if type(stack) is list:
+        stack = np.array(stack)
     stats = stack_describe(stack)
     lims = [s['mean'] + num_std * s['std'] for s in stats]
     tile(stack, bar=bar, clim=zip([0] * len(lims), lims), size=size)
 
 
 def image_lims(im, num_std, bar=True, size=20):
-    from starfish.stats import im_describe
     stats = im_describe(im)
     lim = stats['mean'] + num_std * stats['std']
     image(im, bar=bar, size=size, clim=[0, lim])
-

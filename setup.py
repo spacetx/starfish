c3efc700648d1df2a022832e75572344edd5d77a
Tony Tung <ttung@chanzuckerberg.com>
2017-10-20 12:47:06 -0700

    Fix pip deployment.
    Add a MANIFEST.in so that REQUIREMENTS.txt gets bundled into the package.
    
    Test plan:
    
    ```
    % python setup.py sdist bdist_wheel upload
    ```
    
    then in a different directory, ran:
    ```
    pip install starfish && starfish
    ```

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
+import os
 import setuptools
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "REQUIREMENTS.txt"))]
 
 setuptools.setup(
     name="starfish",
@@ -8,6 +13,7 @@ setuptools.setup(
     author_email="dganguli@chanzuckerberg.com",
     license="MIT",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     entry_points={
         'console_scripts': "starfish=starfish.starfish:starfish"
     }

e16c03784fbb4e8211c7b9557580c7f467717ec9
Tony Tung <tonytung@merly.org>
2018-01-23 14:21:16 -0800

    switch to using argparse instead of click. (#30)

---
+++ starfish/util/argparse.py
@@ -0,0 +1,9 @@
+import argparse
+import os
+
+
+class FsExistsType(object):
+    def __call__(self, prospective_dir):
+        if not os.path.exists(prospective_dir):
+            raise argparse.ArgumentTypeError("{0} does not exist".format(prospective_dir))
+        return prospective_dir

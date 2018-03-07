c19fea0273c14afe73e1a469952c7fcafa687317
Tony Tung <ttung@chanzuckerberg.com>
2018-03-06 17:03:56 -0800

    RFC: Arrange the data according to the proposed new input data format.

--- examples/get_iss_data.py
+++ examples/get_iss_data.py
@@ -1,12 +1,15 @@
 from __future__ import division
 
+import argparse
 import io
+import os
 import json
 import zipfile
 
-import click
 import requests
 from skimage.io import imread, imsave
+
+from starfish.util.argparse import FsExistsType
 
 
 def download(input_dir, url):
@@ -16,97 +19,121 @@ def download(input_dir, url):
     z.extractall(input_dir)
 
 
-def load_tiff_stack(path):
-    stack = imread(path)
-    return stack
+def build_hybridization_stack(input_dir, output_dir):
+    prefix = "fov_0"
 
+    imgs = list()
+    for hyb in range(1, 5):
+        hyb_round = list()
+        for ch in range(2, 6):
+            hyb_round.append(imread(os.path.join(input_dir, str(hyb), "c{}.TIF".format(ch))))
+        imgs.append(hyb_round)
 
-def load_hyb_chan(input_dir, hyb, chan):
-    res = load_tiff_stack(input_dir + '{}/c{}.TIF'.format(hyb, chan))
-    return res
+    hybridization_stack = {
+        'version': "0.0.0",
+        'tiles': [],
+    }
 
-
-def load_and_write_data(input_dir, output_dir):
-    prefix = 'fov_0'
-
-    hyb1 = [load_hyb_chan(input_dir, 1, c) for c in [2, 3, 4, 5]]
-    hyb2 = [load_hyb_chan(input_dir, 2, c) for c in [2, 3, 4, 5]]
-    hyb3 = [load_hyb_chan(input_dir, 3, c) for c in [2, 3, 4, 5]]
-    hyb4 = [load_hyb_chan(input_dir, 4, c) for c in [2, 3, 4, 5]]
-
-    dapi = load_tiff_stack(input_dir + 'DO/c1.TIF')
-    dots = load_tiff_stack(input_dir + 'DO/c2.TIF')
-
-    hyb_list = [hyb1, hyb2, hyb3, hyb4]
-
-    res = dict()
-    res['data'] = []
-    res['aux'] = []
-    res['metadata'] = dict()
-
-    for h, hyb in enumerate(hyb_list):
+    for h, hyb in enumerate(imgs):
         for c, img in enumerate(hyb):
-            fname = '{}_H_{}_C_{}.tiff'.format(prefix, h, c)
+            fname = "{}_H_{}_C_{}.tiff".format(prefix, h, c)
             path = output_dir + fname
-            d = {'hyb': h,
-                 'ch': c,
-                 'file': fname}
-            res['data'].append(d)
+            d = {
+                "coordinates":
+                    {
+                        "x": 0,
+                        "y": 0,
+                        "hyb": h,
+                        "ch": c
+                    },
+                "file":
+                    fname,
+            }
+            hybridization_stack["tiles"].append(d)
             imsave(path, img)
 
-    dapi_fname = '{}_{}.tiff'.format(prefix, 'dapi')
-    imsave(output_dir + dapi_fname, dapi)
-    res['aux'].append({'type': 'dapi', 'file': dapi_fname, 'format': "TIFF"})
+    if 2 < len(img.shape) > 3:
+        raise ValueError("Images must be 2D or 3D. Found: {}".format(img.shape))
 
-    dots_fname = '{}_{}.tiff'.format(prefix, 'dots')
+    hybridization_stack["legend"] = {
+        "dimensions": ["x", "y", "hyb", "ch"],
+        "default_tile_shape": img.shape,
+        "default_tile_format": "TIFF",
+    }
+
+    return hybridization_stack
+
+
+def build_fov(input_dir, hybridization_stack_name, output_dir):
+    prefix = "fov_0"
+
+    nuclei = imread(input_dir + "DO/c1.TIF")
+    dots = imread(input_dir + "DO/c2.TIF")
+
+    experiment = {
+        'version': "0.0.0",
+        'hybridization': hybridization_stack_name,
+        'aux_img': {},
+    }
+
+    nuclei_fname = "{}_{}.tiff".format(prefix, "nuclei")
+    imsave(output_dir + nuclei_fname, nuclei)
+    experiment['aux_img']['nuclei'] = {
+        'file': nuclei_fname,
+        'tile_shape': nuclei.shape,
+        'tile_format': "TIFF",
+    }
+
+    dots_fname = "{}_{}.tiff".format(prefix, "dots")
     imsave(output_dir + dots_fname, dots)
-    res['aux'].append({'type': 'dots', 'file': dots_fname, 'format': "TIFF"})
+    experiment['aux_img']['dots'] = {
+        'file': dots_fname,
+        'tile_shape': dots.shape,
+        'tile_format': "TIFF",
+    }
 
-    if len(img.shape) == 2:
-        is_volume = False
-    elif len(img.shape) == 3:
-        is_volume = True
-    else:
-        raise ValueError('Images must be 2D or 3D. Found: {}'.format(img.shape))
-
-    res['metadata']['num_hybs'] = 4
-    res['metadata']['num_chs'] = 4
-    res['metadata']['shape'] = img.shape
-    res['metadata']['is_volume'] = is_volume
-    res['metadata']['format'] = "TIFF"
-
-    return res
+    return experiment
 
 
-def write_json(res, output_dir):
-    org_json = json.dumps(res, indent=4)
-    print(org_json)
-    fname = output_dir + 'org.json'
-    print("Writing org.json to: {}".format(fname))
-    with open(fname, 'w') as outfile:
+def write_json(res, output_path):
+    json_doc = json.dumps(res, indent=4)
+    print(json_doc)
+    print("Writing to: {}".format(output_path))
+    with open(output_path, "w") as outfile:
         json.dump(res, outfile, indent=4)
 
 
-@click.command()
-@click.argument('input_dir', type=click.Path(exists=True))
-@click.argument('output_dir', type=click.Path(exists=True))
-@click.option('--d', default=False, help='Download data', type=bool)
 def format_data(input_dir, output_dir, d):
-    if not input_dir.endswith('/'):
-        input_dir += '/'
+    if not input_dir.endswith("/"):
+        input_dir += "/"
 
-    if not output_dir.endswith('/'):
-        output_dir += '/'
+    if not output_dir.endswith("/"):
+        output_dir += "/"
 
     if d:
         url = "http://d1zymp9ayga15t.cloudfront.net/content/Examplezips/ExampleInSituSequencing.zip"
         download(input_dir, url)
-        input_dir += 'ExampleInSituSequencing/'
+        input_dir += "ExampleInSituSequencing/"
         print("Data downloaded to: {}".format(input_dir))
+    else:
+        input_dir += "ExampleInSituSequencing/"
+        print("Using data in : {}".format(input_dir))
 
-    res = load_and_write_data(input_dir, output_dir)
-    write_json(res, output_dir)
+    image_stack = build_hybridization_stack(input_dir, output_dir)
+    image_stack_name = "hybridization.json"
+    write_json(image_stack, os.path.join(output_dir, image_stack_name))
+
+    starfish_input = build_fov(input_dir, image_stack_name, output_dir)
+    starfish_input_name = "experiment.json"
+    write_json(starfish_input, os.path.join(output_dir, starfish_input_name))
 
 
-if __name__ == '__main__':
-    format_data()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", type=FsExistsType())
+    parser.add_argument("output_dir", type=FsExistsType())
+    parser.add_argument("--d", help="Download data", type=bool)
+
+    args = parser.parse_args()
+
+    format_data(args.input_dir, args.output_dir, args.d)

e21e1e7c775e914d7bfac373f72f62e9d6e493dc
Tony Tung <ttung@chanzuckerberg.com>
2018-05-17 18:00:34 -0700

    Create yet another base class for all algorithms

--- starfish/pipeline/gene_assignment/_base.py
+++ starfish/pipeline/gene_assignment/_base.py
@@ -1,17 +1,7 @@
-class GeneAssignmentAlgorithm(object):
-    @classmethod
-    def get_algorithm_name(cls):
-        """
-        Returns the name of the algorithm.  This should be a valid python identifier, i.e.,
-        https://docs.python.org/3/reference/lexical_analysis.html#identifiers
-        """
-        raise NotImplementedError()
+from starfish.pipeline.algorithmbase import AlgorithmBase
 
-    @classmethod
-    def add_arguments(cls, parser):
-        """Adds the arguments for the algorithm."""
-        raise NotImplementedError()
 
+class GeneAssignmentAlgorithm(AlgorithmBase):
     def assign_genes(self, spots, regions):
         """Performs gene assignment given the spots and the regions."""
         raise NotImplementedError()
--- starfish/pipeline/pipelinecomponent.py
+++ starfish/pipeline/pipelinecomponent.py
@@ -1,5 +1,4 @@
 import collections
-import functools
 
 
 class PipelineComponent(object):

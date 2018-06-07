51d0a3c2f60d5715d19b6166a7494a61c6119d7f
Tony Tung <ttung@chanzuckerberg.com>
2018-06-07 12:29:00 -0700

    Validate that notebooks can successfully run on master.
    If *.py.skip exists, then that notebook is skipped.
    
    See https://travis-ci.org/spacetx/starfish/builds/389071632 for a run when .travis.yml is set to look for a different branch name.

--- notebooks/subdir.mk
+++ notebooks/subdir.mk
@@ -1,13 +1,18 @@
 py_files := $(wildcard notebooks/*.py)
 ipynb_files := $(wildcard notebooks/*.ipynb)
+py_run_targets := $(addprefix run__, $(py_files))
 ipynb_validate_targets := $(addprefix validate__, $(ipynb_files))
 ipynb_regenerate_targets := $(addprefix regenerate__, $(addsuffix .ipynb, $(basename $(py_files))))
 py_regenerate_targets := $(addprefix regenerate__, $(addsuffix .py, $(basename $(ipynb_files))))
 
 test: $(ipynb_validate_targets)
+run_notebooks: $(py_run_targets)
 validate_notebooks: $(ipynb_validate_targets)
 regenerate_ipynb: $(ipynb_regenerate_targets)
 regenerate_py: $(py_regenerate_targets)
+
+$(py_run_targets): run__%.py :
+	[ -e $*.py.skip ] || python $*.py
 
 $(ipynb_validate_targets): TEMPFILE := $(shell mktemp)
 $(ipynb_validate_targets): validate__%.ipynb :

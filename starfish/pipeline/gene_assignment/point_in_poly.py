8c2e09a10afb528059c9f2bc7bad088494078fa2
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-06-13 22:08:19 -0400

    ENH: move assign code to pipeline component

--- starfish/pipeline/gene_assignment/point_in_poly.py
+++ starfish/pipeline/gene_assignment/point_in_poly.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+from skimage.measure import points_in_poly
+
 from ._base import GeneAssignmentAlgorithm
 
 
@@ -10,8 +14,25 @@ class PointInPoly(GeneAssignmentAlgorithm):
         pass
 
     def assign_genes(self, spots, regions):
-        from starfish.assign import assign
-
         # TODO only works in 3D
         points = spots.loc[:, ['x', 'y']].values
-        return assign(regions, points, use_hull=True)
+        return self._assign(regions, points, use_hull=True)
+
+    @staticmethod
+    def _assign(cells_region, spots, use_hull=True, verbose=False):
+        res = pd.DataFrame({'spot_id': range(0, spots.shape[0])})
+        res['cell_id'] = None
+
+        for cell_id in range(cells_region.count):
+            if use_hull:
+                verts = cells_region[cell_id].hull
+            else:
+                verts = cells_region[cell_id].coordinates
+            verts = np.array(verts)
+            in_poly = points_in_poly(spots, verts)
+            res.loc[res.spot_id[in_poly], 'cell_id'] = cell_id
+            if verbose:
+                cnt = np.sum(in_poly)
+                print(cell_id, cnt)
+
+        return res

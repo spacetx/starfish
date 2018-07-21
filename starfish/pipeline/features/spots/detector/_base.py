bd28325b6049c1186928d6b58c0d43a5b5c16ac2
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-07-21 11:54:37 -0400

    DEP: remove unused EncodedSpots and DecodedSpots

--- starfish/pipeline/features/spots/detector/_base.py
+++ starfish/pipeline/features/spots/detector/_base.py
@@ -1,12 +1,9 @@
-from typing import Tuple
-
 from starfish.image import ImageStack
 from starfish.pipeline.algorithmbase import AlgorithmBase
-from starfish.pipeline.features.spot_attributes import SpotAttributes
-from starfish.pipeline.features.encoded_spots import EncodedSpots
 
 
 class SpotFinderAlgorithmBase(AlgorithmBase):
-    def find(self, hybridization_image: ImageStack) -> Tuple[SpotAttributes, EncodedSpots]:
+    # TODO ambrosejcarr: should return IntensityTable when refactor is complete
+    def find(self, hybridization_image: ImageStack):
         """Find spots."""
         raise NotImplementedError()

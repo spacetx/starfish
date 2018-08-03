1fdf2d8a4c910a1bb1f79b87bb81d5e1d734e545
Deep Ganguli <deep.ganguli@gmail.com>
2018-08-03 14:53:40 -0700

    no whitespace around = for default parameters, man.

--- starfish/pipeline/features/pixels/pixel_spot_detector.py
+++ starfish/pipeline/features/pixels/pixel_spot_detector.py
@@ -11,8 +11,8 @@ from ._base import PixelFinderAlgorithmBase
 class PixelSpotDetector(PixelFinderAlgorithmBase):
     def __init__(
             self, codebook: Codebook, metric: str, distance_threshold: float,
-            magnitude_threshold: int, min_area: int, max_area: int, norm_order: int = 2,
-            crop_x: int = 0, crop_y: int = 0, crop_z: int = 0, **kwargs) -> None:
+            magnitude_threshold: int, min_area: int, max_area: int, norm_order: int=2,
+            crop_x: int=0, crop_y: int=0, crop_z: int=0, **kwargs) -> None:
         """Decode an image by first coding each pixel, then combining the results into spots
 
         Parameters

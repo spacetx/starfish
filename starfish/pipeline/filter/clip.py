8aeaa7cb55925bbb8638356ecb86bfb3108534ff
Ambrose J Carr <ambrosejcarr@users.noreply.github.com>
2018-08-06 17:00:36 -0700

    ENH: harmonize clip API (#360)

--- starfish/pipeline/filter/clip.py
+++ starfish/pipeline/filter/clip.py
@@ -9,51 +9,52 @@ from ._base import FilterAlgorithmBase
 
 class Clip(FilterAlgorithmBase):
 
-    def __init__(self, p_min, p_max, is_volume: bool=False, verbose: bool=False, **kwargs) -> None:
+    def __init__(self, p_min: int=0, p_max: int=100, is_volume: bool=False, **kwargs) -> None:
         """Image clipping filter
 
         Parameters
         ----------
         p_min : int
-            values below this percentile are set to p_min
+            values below this percentile are set to p_min (default 0)
         p_max : int
-            values above this percentile are set to p_max
+            values above this percentile are set to p_max (default 100)
         is_volume : bool
-            If True, 3d (z, y, x) volumes will be passed to self.low_pass. By default, 2d tiles will be passed
-        verbose : bool
-            If True, report on the percentage completed (default = False) during processing
+            If True, 3d (z, y, x) volumes will be filtered. By default, filter 2-d (y, x) tiles
+
         kwargs
         """
-        self.p_min: int = p_min
-        self.p_max: int = p_max
+        self.p_min = p_min
+        self.p_max = p_max
         self.is_volume = is_volume
-        self.verbose = verbose
 
     @classmethod
-    def add_arguments(cls, group_parser):
-        group_parser.add_argument("--p-min", default=0, type=int, help="clip intensities below this percentile")
-        group_parser.add_argument("--p-max", default=100, type=int, help="clip intensities above this percentile")
+    def add_arguments(cls, group_parser) -> None:
+        group_parser.add_argument(
+            "--p-min", default=0, type=int, help="clip intensities below this percentile")
+        group_parser.add_argument(
+            "--p-max", default=100, type=int, help="clip intensities above this percentile")
 
     @staticmethod
-    def clip(image, p_min: int, p_max: int) -> np.ndarray:
+    def clip(image: np.ndarray, p_min: int, p_max: int) -> np.ndarray:
         """Clip values of img below and above percentiles p_min and p_max
 
         Parameters
         ----------
-        p_min, int (default 0)
+        image : np.ndarray
+            image to be clipped
+        p_min : int
           values below this percentile are set to the value of this percentile
-        p_max, int (default 100)
+        p_max : int
           values above this percentile are set to the value of this percentile
 
         Notes
         -----
         - Wrapper for np.clip
-        - No shifting or transformation to adjust dynamic range is done after
-          clipping
+        - No shifting or transformation to adjust dynamic range is done after clipping
 
         Returns
         -------
-        np.ndarra
+        np.ndarray :
           Numpy array of same shape as img
 
         """
@@ -65,7 +66,9 @@ class Clip(FilterAlgorithmBase):
         image = image.clip(min=v_min, max=v_max)
         return image.astype(dtype)
 
-    def run(self, stack: ImageStack, in_place: bool=True) -> Optional[ImageStack]:
+    def run(
+            self, stack: ImageStack, in_place: bool=True, verbose: bool=False
+    ) -> Optional[ImageStack]:
         """Perform filtering of an image stack
 
         Parameters
@@ -74,6 +77,8 @@ class Clip(FilterAlgorithmBase):
             Stack to be filtered.
         in_place : bool
             if True, process ImageStack in-place, otherwise return a new stack
+        verbose : bool
+            If True, report on the percentage completed (default = False) during processing
 
         Returns
         -------
@@ -82,7 +87,7 @@ class Clip(FilterAlgorithmBase):
 
         """
         clip = partial(self.clip, p_min=self.p_min, p_max=self.p_max)
-        result = stack.apply(clip, is_volume=self.is_volume, verbose=self.verbose, in_place=in_place)
+        result = stack.apply(clip, is_volume=self.is_volume, verbose=verbose, in_place=in_place)
         if not in_place:
             return result
         return None

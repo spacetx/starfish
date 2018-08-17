7231ed13176fb588df9d15aabd5f866796398f68
Tony Tung <ttung@chanzuckerberg.com>
2018-08-17 13:13:06 -0700

    Create entry point for starfish/experiment/builder/cli.py
    1. Create a "main" method for the script.
    2. Add an entry point for the pip package.
    
    Test plan:
    ```
    % pip install -e .
    % build_sample_experiment --help
    ```

--- starfish/experiment/builder/cli.py
+++ starfish/experiment/builder/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from typing import MutableMapping
 
 from starfish.types import Indices
 from starfish.util.argparse import FsExistsType
@@ -21,43 +22,53 @@ class StarfishIndex:
         }
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "output_dir",
-        type=FsExistsType())
-    parser.add_argument(
-        "--fov-count",
-        type=int,
-        required=True,
-        help="Number of FOVs in this experiment.")
-    parser.add_argument(
-        "--hybridization-dimensions",
-        type=StarfishIndex(),
-        required=True,
-        help="Dimensions for the hybridization images.  Should be a json dict, with {}, {}, and {} "
-             "as the possible keys.  The value should be the shape along that dimension.  If a key "
-             "is not present, the value is assumed to be 0.".format(
-            Indices.ROUND.value,
-            Indices.CH.value,
-            Indices.Z.value))
-    name_arg_map = dict()
-    for aux_image_name in AUX_IMAGE_NAMES:
-        arg = parser.add_argument(
-            "--{}-dimensions".format(aux_image_name),
+class Cli:
+    parser_group = None
+
+    """
+    Maps the name of an aux image to the name in the parsed arguments object.
+    """
+    name_arg_map: MutableMapping[str, str] = dict()
+
+    @staticmethod
+    def add_to_parser(parser):
+        parser.add_argument(
+            "output_dir",
+            type=FsExistsType())
+        parser.add_argument(
+            "--fov-count",
+            type=int,
+            required=True,
+            help="Number of FOVs in this experiment.")
+        parser.add_argument(
+            "--hybridization-dimensions",
             type=StarfishIndex(),
-            help="Dimensions for the {} images.  Should be a json dict, with {}, {}, and {} as the "
-                 "possible keys.  The value should be the shape along that dimension.  If a key is "
-                 "not present, the value is assumed to be 0.".format(
-                aux_image_name, Indices.ROUND, Indices.CH, Indices.Z))
-        name_arg_map[aux_image_name] = arg.dest
+            required=True,
+            help="Dimensions for the hybridization images.  Should be a json dict, with {}, {}, "
+                 "and {} as the possible keys.  The value should be the shape along that "
+                 "dimension.  If a key is not present, the value is assumed to be 0.".format(
+                Indices.ROUND.value,
+                Indices.CH.value,
+                Indices.Z.value))
+        for aux_image_name in AUX_IMAGE_NAMES:
+            arg = parser.add_argument(
+                "--{}-dimensions".format(aux_image_name),
+                type=StarfishIndex(),
+                help="Dimensions for the {} images.  Should be a json dict, with {}, {}, and {} as "
+                     "the possible keys.  The value should be the shape along that dimension.  If "
+                     "a key is not present, the value is assumed to be 0.".format(
+                    aux_image_name, Indices.ROUND, Indices.CH, Indices.Z))
+            Cli.name_arg_map[aux_image_name] = arg.dest
+        parser.set_defaults(starfish_command=Cli.run)
 
-    args = parser.parse_args()
+        Cli.parser_group = parser
 
-    write_experiment_json(
-        args.output_dir, args.fov_count, args.hybridization_dimensions,
-        {
-            aux_image_name: getattr(args, name_arg_map[aux_image_name])
-            for aux_image_name in AUX_IMAGE_NAMES
-        }
-    )
+    @staticmethod
+    def run(args, print_help=False):
+        write_experiment_json(
+            args.output_dir, args.fov_count, args.hybridization_dimensions,
+            {
+                aux_image_name: getattr(args, Cli.name_arg_map[aux_image_name])
+                for aux_image_name in AUX_IMAGE_NAMES
+            }
+        )

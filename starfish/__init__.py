95bfd476191b1342c0ac2bfd83d32ccf3b396892
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-08-17 16:23:44 -0400

    tonyfix

--- starfish/__init__.py
+++ starfish/__init__.py
@@ -2,8 +2,8 @@
 # See: https://stackoverflow.com/questions/40845304/
 #      runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility)
 import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")  # noqa
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")  # noqa
 
 # image processing methods and objects
 from . import image

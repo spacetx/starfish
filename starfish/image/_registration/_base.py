09662ef43c188372037507a7570270728de5e097
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-08-18 09:10:02 -0400

    ENH: enable out-of-place-registration

--- starfish/image/_registration/_base.py
+++ starfish/image/_registration/_base.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from starfish.pipeline.algorithmbase import AlgorithmBase
 from starfish.stack import ImageStack
 
 
 class RegistrationAlgorithmBase(AlgorithmBase):
-    def run(self, stack) -> ImageStack:
+    def run(self, stack) -> Optional[ImageStack]:
         """Performs registration on the stack provided."""
         raise NotImplementedError()

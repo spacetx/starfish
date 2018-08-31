93ab2276c2a9c1297a1b53fcf8d492c416877fb2
Ambrose J Carr <ambrosejcarr@users.noreply.github.com>
2018-08-30 21:30:40 -0400

    ENH: Add basic matplotlib visualization suite.  (#491)

---
+++ starfish/plot/style.py
@@ -0,0 +1,12 @@
+"""
+This module contains default style constants for visualization methods in starfish
+"""
+
+import matplotlib.pyplot as plt
+
+TARGETS_COLORMAP = plt.get_cmap('nipy_spectral')
+
+BACKGROUND_COLORMAP = plt.get_cmap('gray')
+
+# TODO ambrosejcarr: propagate this through plots
+FONTSIZE = 14

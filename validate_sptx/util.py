8ce3a14c3025ec67ae1dee3eacf413d4e0f77f02
jmoore <josh@glencoesoftware.com>
2018-09-07 09:31:43 +0100

    Add type annotations and docs

--- validate_sptx/util.py
+++ validate_sptx/util.py
@@ -5,7 +5,7 @@ import sys
 import warnings
 
 from pkg_resources import resource_filename
-from typing import Dict, Iterator
+from typing import Any, Dict, IO, Iterator, List, Optional
 
 from jsonschema import RefResolver, Draft4Validator, ValidationError
 
@@ -144,86 +144,91 @@ class SpaceTxValidator:
 
 class Fuzzer(object):
 
-    def __init__(self, validator, obj, out=sys.stdout):
+    def __init__(self, validator : Draft4Validator, obj : Any, out : IO=sys.stdout) -> None:
+        """create a fuzzer which will check different situations against the validator
+
+        Parameters
+        ----------
+        validator : SpaceTxValidator
+            validator which should match the given object type
+        obj : Any
+            JSON-like object which will be checked against the validator
+        out : IO
+            if true, then the json documents which are validated will
+            be modified piece-wise and a statement printed to standard
+            out about whether or not they are still valid.
+
+        """
         self.validator = validator
         self.obj = obj
         self.out = out
-        self.stack = None
+        self.stack : Optional[List[Any]] = None
 
-    def fuzz(self):
+    def fuzz(self) -> None:
+        """prints to the out field the state of the object tree after types of fuzzing
+
+        Each line is prefixed by the output of {state()} followed by a YAML-like
+        representation of the branch of the object tree.
+        """
         header = f"{self.state()}"
         header += "If the letter is present, mutation is valid!"
         self.out.write(f"{header}\n")
         self.out.write("".join([x in ("\t", "\n") and x or "-" for x in header]))
         self.out.write("\n")
-        self.stack = []
+        self.stack : List[Any] = []
         try:
-            self._descend(self.obj)
+            self.descend(self.obj)
         finally:
             self.stack = None
 
-    def state(self):
+    def state(self) -> str:
+        """primary driver for the checks of individual trees
+
+        Returns
+        -------
+        str :
+            space-separated representation of the fuzzing conditions.
+            If a letter is present, then mutation leaves the tree in
+            a valid state:
+
+             A: inserting a fake key or appending to a list
+             D: deleting a key or index
+             I: converting value to an integer
+             I: converting value to a string
+             M: converting value to an empty dict
+             L: converting value to an empty list
+
+        """
         rv = [
-            self.Add().check(self),
-            self.Del().check(self),
-            self.Change("I", lambda *args: 123456789).check(self),
-            self.Change("S", lambda *args: "fake").check(self),
-            self.Change("M", lambda *args: dict()).check(self),
-            self.Change("L", lambda *args: list()).check(self),
+            Add().check(self),
+            Del().check(self),
+            Change("I", lambda *args: 123456789).check(self),
+            Change("S", lambda *args: "fake").check(self),
+            Change("M", lambda *args: dict()).check(self),
+            Change("L", lambda *args: list()).check(self),
         ]
         return ' '.join(rv) + "\t"
 
-    class Checker(object):
+    def descend(self, obj : Any, depth : int=0, prefix : str="") -> None:
+        """walk a JSON-like object tree printing the state of the tree
+        at each level. A YAML representation is used for simplicity.
 
-        def check(self, fuzz):
-            # Don't mess with the top level
-            if fuzz.stack is None: return self.LETTER
-            if not fuzz.stack: return "-"
-            # Operate on a copy for mutating
-            dupe = copy.deepcopy(fuzz.obj)
-            target = dupe
-            for level in fuzz.stack[0:-1]:
-                target = target.__getitem__(level)
-            self.handle(fuzz, target)
-            valid = fuzz.validator.is_valid(dupe)
-            return valid and self.LETTER or "."
-
-    class Add(Checker):
-        LETTER = "A"
-        def handle(self, fuzz, target):
-            if isinstance(target, dict):
-                target["fake"] = "!"
-            elif isinstance(target, list):
-                target.append("!")
-            else:
-                raise Exception("unknown")
-
-    class Del(Checker):
-        LETTER = "D"
-        def handle(self, fuzz, target):
-            key = fuzz.stack[-1]
-            if isinstance(key, tuple):
-                key = key[0]
-            target.__delitem__(key)
-
-    class Change(Checker):
-
-        def __init__(self, letter, call):
-            self.LETTER = letter
-            self.call = call
-
-        def handle(self, fuzz, target):
-            key = fuzz.stack[-1]
-            if isinstance(key, tuple):
-                key = key[0]
-            target.__setitem__(key, self.call())
-
-    def _descend(self, obj, depth=0, prefix=""):
+        Parameters
+        ----------
+        obj : Any
+            JSON-like object tree
+        depth : int
+            depth in the tree that is currently being evaluated
+        prefix : str
+            value which should be prepended to printouts at this level
+        """
+        if self.stack is None:
+            raise Exception("invalid state")
         if isinstance(obj, list):
             for i, o in enumerate(obj):
                 depth += 1
                 self.stack.append(i)
-                self._descend(o, depth, prefix="- ")
+                self.descend(o, depth, prefix="- ")
                 self.stack.pop()
                 depth -= 1
         elif isinstance(obj, dict):
@@ -236,8 +241,75 @@ class Fuzzer(object):
                 if prefix == "- ": prefix = "  "
                 depth += 1
                 self.stack.append(k)
-                self._descend(obj[k], depth, prefix="  "+prefix)
+                self.descend(obj[k], depth, prefix="  "+prefix)
                 self.stack.pop()
                 depth -= 1
         else:
             self.out.write(f"{self.state()}{' ' * depth}{prefix}{obj}\n")
+
+class Checker(object):
+    LETTER = "?"
+
+    def check(self, fuzz : Fuzzer) -> str:
+        """create a copy of the current state of the object tree,
+        mutate it, and run it through is_valid on the validator.
+
+        Parameters
+        ----------
+        fuzz : Fuzzer
+            the containing instance
+
+        Returns
+        -------
+        str :
+            A single character string representation of the check
+
+        """
+        # Don't mess with the top level
+        if fuzz.stack is None: return self.LETTER
+        if not fuzz.stack: return "-"
+        # Operate on a copy for mutating
+        dupe = copy.deepcopy(fuzz.obj)
+        target = dupe
+        for level in fuzz.stack[0:-1]:
+            target = target.__getitem__(level)
+        self.handle(fuzz, target)
+        valid = fuzz.validator.is_valid(dupe)
+        return valid and self.LETTER or "."
+
+    def handle(self, fuzz, target):
+       raise NotImplementedError()
+
+class Add(Checker):
+
+    LETTER = "A"
+
+    def handle(self, fuzz, target):
+        if isinstance(target, dict):
+            target["fake"] = "!"
+        elif isinstance(target, list):
+            target.append("!")
+        else:
+            raise Exception("unknown")
+
+class Del(Checker):
+
+    LETTER = "D"
+
+    def handle(self, fuzz, target):
+        key = fuzz.stack[-1]
+        if isinstance(key, tuple):
+            key = key[0]
+        target.__delitem__(key)
+
+class Change(Checker):
+
+    def __init__(self, letter, call):
+        self.LETTER = letter
+        self.call = call
+
+    def handle(self, fuzz, target):
+        key = fuzz.stack[-1]
+        if isinstance(key, tuple):
+            key = key[0]
+        target.__setitem__(key, self.call())

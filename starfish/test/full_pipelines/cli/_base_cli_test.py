c1e8509e50081100e7f472b27b09c1f54cd3aab3
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-09-14 16:13:59 -0700

    refactored _base_cli_test to use starfish.util.exec

--- starfish/test/full_pipelines/cli/_base_cli_test.py
+++ starfish/test/full_pipelines/cli/_base_cli_test.py
@@ -1,19 +1,18 @@
 import os
 import shutil
-import subprocess
-import tempfile
 import unittest
 from typing import List, Tuple
 
 from starfish.intensity_table import IntensityTable
-from starfish.util import clock
+from starfish.util import exec
 
 
 class CLITest(unittest.TestCase):
-    """This is a base class for testing CLI methods. Each stage should correspond to a different pipeline step.
-    Running the test will go through each stage and run the command line method with given arguments.
-    The last stage should produce a file called results containing an IntensityTable.
-    Each cli test should define it's own verify_results method.
+    """This is a base class for testing CLI methods. Each stage should correspond
+    to a different pipeline step. Running the test will go through each stage and
+    run the command line method with given arguments. The last stage should produce
+    a file called results containing an IntensityTable. Each cli test should define
+    it's own verify_results method.
     """
     # Since this is a base class we don't want to actually run it as a test.
     # #For classes that inherit CLITest this will need to be set to True.
@@ -27,36 +26,9 @@ class CLITest(unittest.TestCase):
         raise NotImplementedError()
 
     def test_run_pipline(self):
-        tempdir = tempfile.mkdtemp()
-        coverage_enabled = "STARFISH_COVERAGE" in os.environ
-        print(tempdir)
+        tempdir = exec.stages(self.STAGES, self.SUBDIRS, keep_data=True)
+        intensities = IntensityTable.load(os.path.join(tempdir, "results", self.spots_file))
+        self.verify_results(intensities)
 
-        def callback(interval):
-            print(" ".join(stage[:2]), " ==> {} seconds".format(interval))
-
-        try:
-            for subdir in self.SUBDIRS:
-                os.makedirs("{tempdir}".format(
-                    tempdir=os.path.join(tempdir, subdir)))
-            for i, stage in enumerate(self.STAGES):
-                cmdline = [
-                    element(tempdir=tempdir) if callable(element) else element
-                    for element in stage
-                ]
-                if cmdline[0] == "starfish" and coverage_enabled:
-                    coverage_cmdline = [
-                        "coverage", "run",
-                        "-p",
-                        "--source", "starfish",
-                        "-m", "starfish",
-                    ]
-                    coverage_cmdline.extend(cmdline[1:])
-                    cmdline = coverage_cmdline
-                with clock.timeit(callback):
-                    subprocess.check_call(cmdline)
-            intensities = IntensityTable.load(os.path.join(tempdir, "results", self.spots_file))
-            self.verify_results(intensities)
-
-        finally:
-            if os.getenv("TEST_KEEP_DATA") is None:
-                shutil.rmtree(tempdir)
+        if os.getenv("TEST_KEEP_DATA") is None:
+            shutil.rmtree(tempdir)

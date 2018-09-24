f06a8c959ef6f70b90cb09dcbc38cf84edad0a5b
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-09-24 15:57:12 -0700

    fixing pep8 error

--- starfish/data/__init__.py
+++ starfish/data/__init__.py
@@ -2,16 +2,21 @@ from starfish import Experiment
 
 
 def MERFISH():
-    return Experiment.from_json('https://dmf0bdeheu4zf.cloudfront.net/20180919/MERFISH/experiment.json')
+    return Experiment.from_json(
+        'https://dmf0bdeheu4zf.cloudfront.net/20180919/MERFISH/experiment.json')
 
 def allen_smFISH():
-    return Experiment.from_json('https://dmf0bdeheu4zf.cloudfront.net/20180919/allen_smFISH/experiment.json')
+    return Experiment.from_json(
+        'https://dmf0bdeheu4zf.cloudfront.net/20180919/allen_smFISH/experiment.json')
 
 def DARTFISH():
-    return Experiment.from_json('https://dmf0bdeheu4zf.cloudfront.net/20180919/DARTFISH/experiment.json')
+    return Experiment.from_json(
+        'https://dmf0bdeheu4zf.cloudfront.net/20180919/DARTFISH/experiment.json')
 
 def ISS():
-    return Experiment.from_json('https://dmf0bdeheu4zf.cloudfront.net/20180919/ISS/experiment.json')
+    return Experiment.from_json(
+        'https://dmf0bdeheu4zf.cloudfront.net/20180919/ISS/experiment.json')
 
 def osmFISH():
-    return Experiment.from_json('https://dmf0bdeheu4zf.cloudfront.net/20180919/osmFISH/experiment.json')
+    return Experiment.from_json(
+        'https://dmf0bdeheu4zf.cloudfront.net/20180919/osmFISH/experiment.json')

1d524165ecf64e8ae122454057ca8b821362aa94
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-09-25 16:18:11 -0700

    adding testing file

---
+++ starfish/test/full_pipelines/api/test_iss_doc_pipeline.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+import starfish
+
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(starfish.__file__)))
+os.environ["USE_TEST_DATA"] = "1"
+sys.path.append(os.path.join(ROOT_DIR, "docs", "source", "usage", "iss"))
+
+
+def test_iss_pipeline_in_docs():
+    # Just importing the file and verifying it runs for now
+
+    iss = __import__('iss_pipeline')

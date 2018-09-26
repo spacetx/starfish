3ac2a36dec2980509be5b5623394a89843cdab41
kevinyamauchi <kevin.yamauchi@gmail.com>
2018-09-26 00:42:15 -0700

    Modernize MeanHighPass (#620)
    * mean high pass type check and doc
    
    * updated kernal shape check
    
    * fixed lint errors
    
    * fixed import spacing
    
    * fixed high_pass() types

--- starfish/image/_filter/mean_high_pass.py
+++ starfish/image/_filter/mean_high_pass.py
@@ -4,13 +4,11 @@ from typing import Callable, Optional, Tuple, Union
 
 import numpy as np
 from scipy.ndimage.filters import uniform_filter
-from skimage import img_as_uint
 
-from starfish.errors import DataFormatWarning
 from starfish.imagestack.imagestack import ImageStack
 from starfish.types import Number
 from ._base import FilterAlgorithmBase
-from .util import preserve_float_range
+from .util import preserve_float_range, validate_and_broadcast_kernel_size
 
 
 class MeanHighPass(FilterAlgorithmBase):
@@ -38,15 +36,7 @@ class MeanHighPass(FilterAlgorithmBase):
 
         """
 
-        if isinstance(size, tuple):
-            message = ("if passing an anisotropic kernel, the dimensionality must match the data "
-                       "shape ({shape}), not {passed_shape}")
-            if is_volume and len(size) != 3:
-                raise ValueError(message.format(shape=3, passed_shape=len(size)))
-            if not is_volume and len(size) != 2:
-                raise ValueError(message.format(shape=2, passed_shape=len(size)))
-
-        self.size = size
+        self.size = validate_and_broadcast_kernel_size(size, is_volume)
         self.is_volume = is_volume
 
     @classmethod
@@ -60,28 +50,24 @@ class MeanHighPass(FilterAlgorithmBase):
     @staticmethod
     def high_pass(image: np.ndarray, size: Number, rescale: bool=False) -> np.ndarray:
         """
-        Applies a gaussian high pass filter to an image
+        Applies a mean high pass filter to an image
 
         Parameters
         ----------
-        image : numpy.ndarray[np.uint16]
+        image : numpy.ndarray[np.float32]
             2-d or 3-d image data
-        size : Number
+        size : Union[Number, Tuple[Number]]
             width of the kernel
         rescale : bool
             If true scales data by max value, if false clips max values to one
 
         Returns
         -------
-        np.ndarray :
+        np.ndarray [np.float32]:
             Filtered image, same shape as input
             :param clip:
 
         """
-        if image.dtype != np.uint16:
-            DataFormatWarning(
-                "Mean filters currently only support uint16 images. Image data will be converted.")
-            image = img_as_uint(image)
 
         blurred: np.ndarray = uniform_filter(image, size)
 

48ab0be36ccb7bf51f23c59fa7995bcaa7644a5d
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-09-27 09:39:32 -0700

    changing to posixpath.join()

--- examples/get_cli_test_data.py
+++ examples/get_cli_test_data.py
@@ -1,7 +1,7 @@
 import argparse
 import json
-import os
 import pathlib
+import posixpath
 
 import requests
 
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # save image stacks locally
-    exp = Experiment.from_json(os.path.join(args.experiment_url, "experiment.json"))
+    exp = Experiment.from_json(posixpath.join(args.experiment_url, "experiment.json"))
 
     for fov in exp.fovs():
         fov_dir = pathlib.Path(args.output_dir, fov.name)
@@ -26,13 +26,13 @@ if __name__ == "__main__":
             fov[image_type].write(str(fov_dir / f"{image_type}.json"))
 
     # get codebook from url and save locally to tmp dir
-    codebook = requests.get(os.path.join(args.experiment_url, "codebook.json"))
+    codebook = requests.get(posixpath.join(args.experiment_url, "codebook.json"))
     data = codebook.json()
     with open(pathlib.Path(args.output_dir, 'codebook.json'), 'w') as f:
         json.dump(data, f)
 
     # get experiment.json from url and save locally to tmp dir
-    experiment = requests.get(os.path.join(args.experiment_url, "experiment.json"))
+    experiment = requests.get(posixpath.join(args.experiment_url, "experiment.json"))
     data = experiment.json()
     with open(pathlib.Path(args.output_dir, 'experiment.json'), 'w') as f:
         json.dump(data, f)

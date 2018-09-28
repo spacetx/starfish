ee48d22d03f8a53a86073abfed5bb636d997f311
kevinyamauchi <kevin.yamauchi@gmail.com>
2018-09-28 14:45:28 -0700

    Add tests for mean high pass filter (#655)
    * added mhp test
    
    * made filter imports one line

--- starfish/test/pipeline/test_filter.py
+++ starfish/test/pipeline/test_filter.py
@@ -3,7 +3,7 @@ from typing import Tuple, Union
 import numpy as np
 import pytest
 
-from starfish.image._filter import gaussian_high_pass
+from starfish.image._filter import gaussian_high_pass, mean_high_pass
 from starfish.imagestack.imagestack import ImageStack
 from starfish.types import Number
 
@@ -30,4 +30,23 @@ def test_gaussian_high_pass_3d(sigma: Union[Number, Tuple[Number]]) -> None:
     sum_before = np.sum(image_stack.numpy_array)
     ghp = gaussian_high_pass.GaussianHighPass(sigma=sigma, is_volume=True)
     result = ghp.run(image_stack)
+    assert np.sum(result.numpy_array) < sum_before
+
+@pytest.mark.parametrize('size', (1, (1, 1)))
+def test_mean_high_pass(size: Union[Number, Tuple[Number]]) -> None:
+    """high pass is subtractive, sum of array should be less after running."""
+    image_stack = random_data_image_stack_factory()
+    sum_before = np.sum(image_stack.numpy_array)
+    mhp = mean_high_pass.MeanHighPass(size=size)
+    result = mhp.run(image_stack)
+    assert np.sum(result.numpy_array) < sum_before
+
+
+@pytest.mark.parametrize('size', (1, (1, 0, 1)))
+def test_mean_high_pass_3d(size: Union[Number, Tuple[Number]]) -> None:
+    """same as test_mean_high_pass, but tests apply loop functionality in 3d"""
+    image_stack = random_data_image_stack_factory()
+    sum_before = np.sum(image_stack.numpy_array)
+    mhp = mean_high_pass.MeanHighPass(size=size, is_volume=True)
+    result = mhp.run(image_stack)
     assert np.sum(result.numpy_array) < sum_before

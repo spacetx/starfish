31907d8fd2323d99b67cd82b170e5be9794b7e75
Tony Tung <ttung@chanzuckerberg.com>
2018-10-01 10:43:05 -0700

    Save the coordinates from the tiles to an xarray during ImageStack construction.
    Added a test to verify that the coordinates are saved and retrieved correctly.

--- starfish/types/__init__.py
+++ starfish/types/__init__.py
@@ -1,6 +1,12 @@
 from typing import Union
 
-from ._constants import Coordinates, Features, Indices
+from ._constants import (
+    Coordinates,
+    Features,
+    Indices,
+    PHYSICAL_COORDINATE_DIMENSION,
+    PhysicalCoordinateTypes,
+)
 from ._spot_attributes import SpotAttributes
 
 Number = Union[int, float]

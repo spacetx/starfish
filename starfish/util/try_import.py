b6906e79aeb1890072765eefd82807bca466ee12
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-10-27 10:09:52 -0400

    add purpose to try_import docs

--- starfish/util/try_import.py
+++ starfish/util/try_import.py
@@ -7,6 +7,8 @@ def try_import(allowable_module_names: Optional[Set[str]]=None) -> Callable:
     Decorator to apply to a method.  If one of the modules in `allowable_module_names` fail to
     import, raise a friendly error message.  If `allowable_module_names` is None, then all failed
     imports raise a friendly error message.
+
+    Enables large and peripheral dependencies to be excluded from the build.
     """
     def _try_import_decorator(func):
         @functools.wraps(func)
@@ -19,7 +21,7 @@ def try_import(allowable_module_names: Optional[Set[str]]=None) -> Callable:
                 if allowable_module_names is None or module_name in allowable_module_names:
                     raise ImportError(
                         f"{module_name} is an optional dependency of starfish. Please install "
-                        f"{module_name} and its dependenciesto use this functionality."
+                        f"{module_name} and its dependencies to use this functionality."
                     ) from ex
                 else:
                     raise

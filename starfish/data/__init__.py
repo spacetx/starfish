0f64b1ee6f9d8f34043570714060395fa45a5a73
Ambrose J Carr <ambrosejcarr@users.noreply.github.com>
2018-10-29 23:28:02 -0400

    Add Barista-seq to starfish.data (#752)

--- starfish/data/__init__.py
+++ starfish/data/__init__.py
@@ -33,3 +33,23 @@ def ISS(use_test_data: bool=False):
 def osmFISH(use_test_data: bool=False):
     return Experiment.from_json(
         'https://d2nhj9g34unfro.cloudfront.net/20181005/osmFISH/experiment.json')
+
+
+def BaristaSeq(use_test_data: bool=False) -> Experiment:
+    """Loads a BaristaSeq dataset generated from mouse visual cortex. The extracted field of view
+    comes from an internal layer of V1 (range: 2-5)
+
+    Parameters
+    ----------
+    use_test_data : bool
+        This parameter is not used for this data type, as there is no data of testing size.
+
+    Returns
+    -------
+    Experiment
+        Experiment containing raw image data
+    """
+    return Experiment.from_json(
+        "https://d2nhj9g34unfro.cloudfront.net/browse/formatted/20181028/"
+        "BaristaSeq/cropped_formatted/experiment.json"
+    )

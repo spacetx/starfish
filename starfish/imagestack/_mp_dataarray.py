c6ea209e739f85d65a2bbdd5edf2ed52139ca4f7
Tony Tung <tonytung@merly.org>
2018-10-29 23:38:24 -0700

    Make MPDataArray actually shared-memory (#739)
    https://github.com/spacetx/starfish/pull/737 created the wrapper class for xr.DataArray, but fundamentally, it was still a copy-on-write construct that did not allow multiple writers across processes to operate on it.  This swaps out the underpinnings with the constructs introduced and tested in https://github.com/spacetx/starfish/pull/738, such that processes that we fork out in `ImageStack` can cooperatively work on multiple parts of the tensor concurrently.
    
    Whenever we construct a `MPDataArray`, we now construct a `multiprocessing.Array`, and layer a `numpy` object on top of that memory.  Then we construct a `xr.DataArray` around that `numpy` object.  Fun, right?
    
    We freely copy xr.DataArray around when we want to run `ImageStack.apply` not in-place, so we have a `__deepcopy__` method in `MPDataArray` to copy correctly (i.e., create a new `multiprocessing.Array`, and layer everything on top of that).
    
    We also intercept `xr.DataArray``s copy method, such that if someone attempts to deepcopy the xarray directly, we warn them that it has consequences.
    
    Test plan: Added a few tests:
    1. a test that exercises `xr_deepcopy`
    2. a test that exercises `copy.deepcopy(..)` on an ImageStack.
    3. a test that exercises `copy.deepcopy(..)` on an ImageStack's xarray (which will work but will trigger a warning).

--- starfish/imagestack/_mp_dataarray.py
+++ starfish/imagestack/_mp_dataarray.py
@@ -1,8 +1,12 @@
-import copy
-from typing import Sequence
+import functools
+import warnings
+from collections import OrderedDict
+from multiprocessing import Array as mp_array  # type: ignore
+from typing import Callable, Sequence, Tuple
 
 import numpy as np
 import xarray as xr
+from xarray import Variable
 
 from starfish.types import Number
 
@@ -19,18 +23,20 @@ class MPDataArray:
     multiprocessing.Array object back out of the numpy array or the xarray.  Therefore, we need to
     explicitly maintain a reference to it and keep the two items together.
     """
-    def __init__(self, data: xr.DataArray) -> None:
+    def __init__(self, data: xr.DataArray, backing_mp_array: mp_array) -> None:
         self._data = data
+        self._backing_mp_array = backing_mp_array
 
     @classmethod
     def from_shape_and_dtype(
             cls, shape: Sequence[int], dtype, initial_value: Number=None, *args, **kwargs
     ) -> "MPDataArray":
-        np_array = np.empty(shape=shape, dtype=dtype)
+        np_array, backing_mp_array = np_array_backed_by_mp_array(shape, dtype)
         if initial_value is not None:
             np_array.fill(initial_value)
         xarray = xr.DataArray(np_array, *args, **kwargs)
-        return MPDataArray(xarray)
+        xarray.copy = functools.partial(replacement_copy, xarray.copy)
+        return MPDataArray(xarray, backing_mp_array)
 
     @property
     def data(self) -> xr.DataArray:
@@ -46,4 +52,57 @@ class MPDataArray:
         self._data[key] = value
 
     def __deepcopy__(self, memodict={}):
-        return MPDataArray(copy.deepcopy(self._data, memodict))
+        xarray_copy, backing_mp_array_copy = xr_deepcopy(self._data)
+        return MPDataArray(xarray_copy, backing_mp_array_copy)
+
+
+def np_array_backed_by_mp_array(
+        shape: Sequence[int], dtype) -> Tuple[np.ndarray, mp_array]:
+    """Returns a np_array backed by a multiproceessing.Array buffer."""
+    ctype_type = np.ctypeslib.as_ctypes(np.empty((1,), dtype=np.dtype(dtype))).__class__
+    length = int(np.product(shape))  # the cast to int is required by multiprocessing.Array.
+    backing_array = mp_array(ctype_type, length)
+    unshaped_np_array = np.frombuffer(backing_array.get_obj(), dtype)
+    shaped_np_array = unshaped_np_array.reshape(shape)
+
+    return shaped_np_array, backing_array
+
+
+def xr_deepcopy(source: xr.DataArray) -> Tuple[xr.DataArray, mp_array]:
+    """Replacement for xr.DataArray's deepcopy method.  Returns a deep copy of the input xarray
+    backed by a multiprocessing.Array buffer.
+    """
+    shaped_np_array, backing_array = np_array_backed_by_mp_array(
+        source.variable.shape, source.variable.dtype)
+
+    shaped_np_array[:] = source.variable.data
+
+    variable = Variable(
+        # dims is an immutable tuple, so it doesn't need to be deep-copyied.  see implementation of
+        # Variable.__deepcopy__ for context.
+        source.variable.dims,
+        shaped_np_array,
+        # attrs and encoding are deep copied in the constructor.
+        source.variable.attrs,
+        source.variable.encoding,
+    )
+    coords = OrderedDict((k, v.copy(deep=True))
+                         for k, v in source._coords.items())
+
+    result = xr.DataArray(
+        variable,
+        coords=coords,
+        name=source.name,
+        fastpath=True,
+    )
+
+    return result, backing_array
+
+
+def replacement_copy(orig_copy: Callable, deep=True):
+    if deep:
+        warnings.warn(
+            "Calling deepcopy with ImageStack's xarrays results in arrays that cannot be shared "
+            "across processes.  Use mp_dataarray.xr_deepcopy if you need that behavior."
+        )
+    return orig_copy(deep)

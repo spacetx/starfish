c59e038c454c7c470f11c2a24daf91ca80e734ed
jmoore <josh@glencoesoftware.com>
2018-11-23 10:02:37 +0000

    Simplify synthetic stack call

--- starfish/experiment/test_experiment.py
+++ starfish/experiment/test_experiment.py
@@ -6,10 +6,8 @@ from starfish.util.synthesize import SyntheticData
 def test_fov_order():
     data = SyntheticData()
     codebook = data.codebook()
-    stack1 = ImageStack.synthetic_stack(num_round=5, num_ch=5, num_z=15,
-                                        tile_height=200, tile_width=200)
-    stack2 = ImageStack.synthetic_stack(num_round=5, num_ch=5, num_z=15,
-                                        tile_height=200, tile_width=200)
+    stack1 = ImageStack.synthetic_stack()
+    stack2 = ImageStack.synthetic_stack()
     fovs = [FieldOfView("stack2", {"primary": stack2}),
             FieldOfView("stack1", {"primary": stack1})]
     extras = {"synthetic": True}

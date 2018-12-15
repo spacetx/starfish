9edf906506c963285dda7bd96e0958e35f2e5f07
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-12-14 16:59:45 -0800

    referencing python issue disscussion in doc string

--- starfish/imagestack/__init__.py
+++ starfish/imagestack/__init__.py
@@ -6,6 +6,8 @@ def anonymous_arena_init(self, size, fd=-1):
     """
     Patching multiprocessing.heap.Arena.__init__ so that it uses anonymous memory mapping
     instead of creating a temp file for shared memory amongst subprocess.
+
+    fix from: https://bugs.python.org/issue30919
     """
     self.size = size
     self.fd = fd  # still kept but is not used !

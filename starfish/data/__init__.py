f6bb6b506a82bcd8ddc4829cdb21c327fdc68711
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-12-16 11:34:00 -0500

    add basic data-loading notebook, update starfish.data to supply a SeqFISH FoV

--- starfish/data/__init__.py
+++ starfish/data/__init__.py
@@ -86,6 +86,27 @@ def ImagingMassCytometry(use_test_data: bool=False):
 
 
 def SeqFISH(use_test_data: bool=False):
-    return Experiment.from_json(
-        "https://d2nhj9g34unfro.cloudfront.net/browse/formatted/20181211/seqfish/experiment.json"
+    """Loads a SeqFISH field of view generated from cultured mES cells.
+
+    Parameters
+    ----------
+    use_test_data : bool
+        If true, return a small region of testing data that was visually determined to contain two
+        cells.
+
+    Notes
+    -----
+    SeqFISH fields of view are quite large (12, 5, 29, 2048, 2048) and take up approximately
+    5 gb in memory. Use the non-test data with care.
+
+    See Also
+    --------
+    Manuscript for Intron-SeqFISH: https://doi.org/10.1016/j.cell.2018.05.035
+
+    """
+    suffix = "-TEST" if use_test_data else ""
+    url = (
+        f"https://d2nhj9g34unfro.cloudfront.net/browse/formatted/20181211/"
+        f"seqfish{suffix}/experiment.json"
     )
+    return Experiment.from_json(url)

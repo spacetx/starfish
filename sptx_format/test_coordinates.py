1f01a1427a62b107c3760efcbb90e3e83430a5ae
jmoore <josh@glencoesoftware.com>
2018-12-17 20:05:56 +0000

    Rework coordinates
     * xc and yc may only be arrays of length 2
     * zc may be a scalar of an array of length 2

--- sptx_format/test_coordinates.py
+++ sptx_format/test_coordinates.py
@@ -7,18 +7,22 @@ package_name = "sptx_format"
 coordinates_path = resource_filename(package_name, "schema/field_of_view/tiles/coordinates.json")
 validator = SpaceTxValidator(coordinates_path)
 
-example = resource_filename(package_name, "examples/field_of_view/coordinates%s.json")
+example = resource_filename(package_name, "examples/field_of_view/coordinates_%s.json")
 
-@pytest.mark.parametrize("args", (
-    ("1", True),
-    ("2", True),
-    ("3", True),
-    ("4", False),
+
+@pytest.mark.parametrize("name", (
+    "bad_x_scalar",
+    "bad_x_single",
+    "bad_y_scalar",
+    "bad_y_single",
+    "bad_z_single",
+    "bad_z_triple",
+    "good_max",
+    "good_min",
 ))
-def test_coords(args):
-    index, passes = args
-    filename = example % index
-    if passes:
+def test_coords(name):
+    filename = example % name
+    if "good" in name:
         assert validator.validate_file(filename)
     else:
         with pytest.warns(UserWarning):

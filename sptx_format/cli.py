907d45b58a111a144f5bc76796124561873c2c83
jmoore <josh@glencoesoftware.com>
2018-12-19 07:08:47 +0000

    Add --help text for all top-level commands

--- sptx_format/cli.py
+++ sptx_format/cli.py
@@ -8,7 +8,7 @@ from sptx_format import validate_sptx
 @click.option("--fuzz", is_flag=True)
 @click.pass_context
 def validate(ctx, experiment_json, fuzz):
-    """invokes validate with the parsed commandline arguments"""
+    """validate experiment against the jsonschemas"""
     try:
         valid = validate_sptx.validate(experiment_json, fuzz)
         if valid:

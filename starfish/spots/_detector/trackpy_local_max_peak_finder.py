5e58b7c958616ab457a762e71687feae6313918b
shanaxel42 <shannon.axelrod@chanzuckerberg.com>
2018-12-19 10:20:37 -0800

    Converting pixel index values to ints in TrackpyLocalMaxPeakFinder  (#898)
    * converting pixel index values to ints

--- starfish/spots/_detector/trackpy_local_max_peak_finder.py
+++ starfish/spots/_detector/trackpy_local_max_peak_finder.py
@@ -126,6 +126,10 @@ class TrackpyLocalMaxPeakFinder(SpotFinderAlgorithmBase):
             attributes.columns = new_colnames
 
         attributes['spot_id'] = np.arange(attributes.shape[0])
+        # convert these to int so it can be used to index
+        attributes.x = attributes.x.astype(int)
+        attributes.y = attributes.y.astype(int)
+        attributes.z = attributes.z.astype(int)
         return SpotAttributes(attributes)
 
     def run(

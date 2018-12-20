ba1f17b0d76d68edf46335d2e2b473bab152f026
jmoore <josh@glencoesoftware.com>
2018-12-20 17:09:40 +0000

    Simplify help tests

--- starfish/test/full_pipelines/cli/test_help.py
+++ starfish/test/full_pipelines/cli/test_help.py
@@ -1,20 +1,14 @@
-import difflib
 import subprocess
 import unittest
 
 from starfish.util import exec
 
 
-def assert_diff(actual, expected):
+def assert_contains(actual, expected):
     if isinstance(actual, bytes):
         actual = actual.decode("utf-8")
-    if actual != expected:
-        diff = difflib.ndiff(expected.splitlines(keepends=True),
-                             actual.splitlines(keepends=True))
-        diff = list(diff)
-        for line in diff:
-            print(line, end="")
-        raise Exception("help mismatch!")
+    if expected not in actual:
+        raise Exception(f"counldn't find: 'f{expected}'")
 
 
 class TestHelpReturnCode(unittest.TestCase):
@@ -49,72 +43,16 @@ class TestHelpStandardOut(unittest.TestCase):
 
     def test_first(self):
         actual = subprocess.check_output(["starfish", "--help"])
-        expected = """Usage: starfish [OPTIONS] COMMAND [ARGS]...
-
-Options:
-  --profile
-  --help     Show this message and exit.
-
-Commands:
-  build
-  decode
-  detect_spots
-  filter
-  registration
-  segment
-  target_assignment
-  validate           invokes validate with the parsed commandline...
-  version
-"""
-        assert_diff(actual, expected)
+        expected = """Usage: starfish [OPTIONS] COMMAND [ARGS]..."""
+        assert_contains(actual, expected)
 
     def test_second(self):
         actual = subprocess.check_output(["starfish", "detect_spots", "--help"])
-        expected = """Usage: starfish detect_spots [OPTIONS] COMMAND [ARGS]...
-
-Options:
-  -i, --input PATH                [required]
-  -o, --output TEXT               [required]
-  --blobs-stack TEXT              ImageStack that contains the blobs. Will be
-                                  max-projected across imaging round and
-                                  channel to produce the blobs_image
-  --reference-image-from-max-projection
-                                  Construct a reference image by max
-                                  projecting imaging rounds and channels.
-                                  Spots are found in this image and then
-                                  measured across all images in the input
-                                  stack.
-  --codebook TEXT                 A spaceTx spec-compliant json file that
-                                  describes a three dimensional tensor whose
-                                  values are the expected intensity of a spot
-                                  for each code in each imaging round and each
-                                  color channel.
-  --help                          Show this message and exit.
-
-Commands:
-  BlobDetector
-  LocalMaxPeakFinder
-  PixelSpotDetector
-  TrackpyLocalMaxPeakFinder
-"""
+        expected = """Usage: starfish detect_spots [OPTIONS] COMMAND [ARGS]..."""
         actual = actual.decode("utf-8")
-        assert_diff(actual, expected)
+        assert_contains(actual, expected)
 
     def test_third(self):
         actual = subprocess.check_output(["starfish", "detect_spots", "BlobDetector", "--help"])
-        expected = """Usage: starfish detect_spots BlobDetector [OPTIONS]
-
-Options:
-  --min-sigma INTEGER     Minimum spot size (in standard deviation)
-  --max-sigma INTEGER     Maximum spot size (in standard deviation)
-  --num-sigma INTEGER     Number of sigmas to try
-  --threshold FLOAT       Dots threshold
-  --overlap FLOAT         dots with overlap of greater than this fraction are
-                          combined
-  --show                  display results visually
-  --detector_method TEXT  str ['blob_dog', 'blob_doh', 'blob_log'] name of the
-                          type of detection method used from skimage.feature.
-                          Default: blob_log
-  --help                  Show this message and exit.
-"""
-        assert_diff(actual, expected)
+        expected = """Usage: starfish detect_spots BlobDetector [OPTIONS]"""
+        assert_contains(actual, expected)

b1e9b65afc1913578bfd3f4665a8d4c80fdabc8b
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-12-21 09:46:56 -0800

    fixing conflict

--- starfish/pipeline/algorithmbase.py
+++ starfish/pipeline/algorithmbase.py
@@ -45,6 +45,42 @@ class AlgorithmBaseType(type):
 
 
 class AlgorithmBase(metaclass=AlgorithmBaseType):
+
+    """
+     This is the base class of any algorithm that starfish exposes.
+
+    Subclasses of this base class are paired with subclasses of PipelineComponent. The subclasses of
+    PipelineComponent retrieve subclasses of the paired AlgorithmBase. Together, the two classes
+    enable starfish to expose a paired API and CLI.
+    Examples
+    --------
+
+    PipelineComponent: `starfish.image._segmentation.Segmentation(PipelineComponent)`
+
+    AlgorithmBase: `starfish.image._segmentation._base.SegmentationAlgorithmBase(AlgorithmBase)`
+
+    Implementing Algorithms:
+    - `starfish.image._segmentation.watershed.Watershed(SegmentationAlgorithmBase)`
+
+    This pattern exposes the API as follows:
+
+    `starfish.image.Segmentation.<implementing algorithm (Watershed)>`
+
+    and the CLI as:
+
+    `$> starfish segmentation watershed`
+
+    To create an entirely new group of related algorithms, like `Segmentation`, a new subclass of
+    both `AlgorithmBase` and `PipelineComponent` must be created.
+
+    To add to an existing group of algorithms like "Segmentation", an algorithm implementation must
+    subclass the corresponding subclass of `AlgorithmBase`. In this case,
+    `SegmentationAlgorithmBase`.
+
+    See Also
+    --------
+    starfish.pipeline.pipelinecomponent.py
+    """
     @classmethod
     def _get_algorithm_name(cls):
         """

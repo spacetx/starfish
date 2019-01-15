0066ae9f15837df23d338264e0a466502eac82af
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2019-01-15 14:59:51 -0800

    fixing lint error

--- starfish/multiprocessing/pool.py
+++ starfish/multiprocessing/pool.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 
 class Spool(Pool):
-    """Wrapper class for multiprocessing pool. If n_processes=1 just use map on main thread for debugging
-    purposes """
+    """Wrapper class for multiprocessing pool. If n_processes=1 just use map on main
+    thread for debugging purposes """
 
     def __init__(self, processes: Optional[int]=None, **kwargs):
         Pool.__init__(self, **kwargs)

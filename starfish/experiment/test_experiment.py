dfd44edafb5a19c57cb9973304aa91cb8dd04db5
Tony Tung <ttung@chanzuckerberg.com>
2019-02-04 10:27:47 -0800

    Add support for cropping to the experiment API.
    Test plan:
    ```
    from starfish import data
    from starfish.imagestack.parser.crop import CropParameters
    
    exp = data.ISS()
    c = CropParameters(x_slice=slice(100, 300), y_slice=slice(400, 700))
    exp['fov_001'].get_image('primary', c)
    ```

--- starfish/experiment/test_experiment.py
+++ starfish/experiment/test_experiment.py
@@ -1,5 +1,7 @@
+import starfish.data
 from starfish.experiment.experiment import Experiment, FieldOfView
 from starfish.imagestack.imagestack import ImageStack
+from starfish.imagestack.parser.crop import CropParameters
 from starfish.util.synthesize import SyntheticData
 
 
@@ -14,3 +16,11 @@ def test_fov_order():
     experiment = Experiment(fovs, codebook, extras)
     assert "stack1" == experiment.fov().name
     assert ["stack1", "stack2"] == [x.name for x in experiment.fovs()]
+
+
+def test_crop_experiment():
+    exp = starfish.data.ISS(use_test_data=True)
+    c = CropParameters(x_slice=slice(10, 30), y_slice=slice(40, 70))
+    image = exp['fov_001'].get_image('primary', c)
+    assert image.shape['x'] == 20
+    assert image.shape['y'] == 30

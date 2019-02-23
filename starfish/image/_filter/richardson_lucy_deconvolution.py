add tests, fix scaling not being propagated when in_place=False

starfish/image/_filter/richardson_lucy_deconvolution.py
@@ -8,7 +8,6 @@ from scipy.signal import convolve, fftconvolve
 from starfish.imagestack.imagestack import ImageStack
 from starfish.types import Number
 from starfish.util import click
-from starfish.util.dtype import preserve_float_range
 from ._base import FilterAlgorithmBase
 from .util import (
     determine_axes_to_group_by,
@@ -19,7 +18,7 @@ from .util import (
 class DeconvolvePSF(FilterAlgorithmBase):
 
     def __init__(
-        self, num_iter: int, sigma: Number, is_volume: bool = False, clip_method: int=1
+        self, num_iter: int, sigma: Number, is_volume: bool = False, clip_method: int=0
     ) -> None:
         """Deconvolve a point spread function
 
@@ -182,9 +181,9 @@ class DeconvolvePSF(FilterAlgorithmBase):
     @click.option("--is-volume", is_flag=True,
                   help="indicates that the image stack should be filtered in 3d")
     @click.option(
-        "--clip-method", default=1, type=int,
-        help="method to constrain data to [0,1]. 0: clip, 1: scale by max per chunk, 2: scale "
-             "by max over whole ImageStack")
+        "--clip-method", default=0, type=int,
+        help="method to constrain data to [0,1]. 0: clip, 1: scale by max over whole image, "
+             "2: scale by max per chunk")
     @click.pass_context
     def _cli(ctx, num_iter, sigma, is_volume, clip_method):
         ctx.obj["component"]._cli_run(ctx, DeconvolvePSF(num_iter, sigma, is_volume, clip_method))

starfish/image/_filter/scale_by_percentile.py
@@ -44,7 +44,6 @@ class ScaleByPercentile(FilterAlgorithmBase):
         ----------
         image : Union[xr.DataArray, np.ndarray
             image to be scaled
-
         p : int
             each image in the stack is scaled by this percentile. must be in [0, 100]
 
@@ -106,8 +105,8 @@ class ScaleByPercentile(FilterAlgorithmBase):
         "--is-volume", is_flag=True, help="filter 3D volumes")
     @click.option(
         "--clip-method", default=0, type=int,
-        help="method to constrain data to [0,1]. 0: clip, 1: scale by max per chunk, 2: scale "
-             "by max over whole ImageStack")
+        help="method to constrain data to [0,1]. 0: clip, 1: scale by max over whole image, "
+             "2: scale by max per chunk")
     @click.pass_context
     def _cli(ctx, p, is_volume, clip_method):
         ctx.obj["component"]._cli_run(ctx, ScaleByPercentile(p, is_volume, clip_method))

starfish/test/full_pipelines/api/test_merfish.py
@@ -217,7 +217,7 @@ def test_merfish_pipeline_cropped_data():
 
     # verify that the number of spots are correct
     spots_passing_filters = spot_intensities[Features.PASSES_THRESHOLDS].sum()
-    assert spots_passing_filters == 1410
+    assert spots_passing_filters == 1417
 
     # compare to paper results
     bench = pd.read_csv('https://d2nhj9g34unfro.cloudfront.net/MERFISH/benchmark_results.csv',
@@ -232,8 +232,8 @@ def test_merfish_pipeline_cropped_data():
 
     # assert that number of high-expression detected genes are correct
     expected_counts = pd.Series(
-        [113, 105, 57, 44, 30],
-        index=('nan', 'MALAT1', 'SRRM2', 'FASN', 'LRP1')
+        [120, 101, 54, 43, 30],
+        index=('nan', 'MALAT1', 'SRRM2', 'FASN', 'PRKDC')
     )
     assert np.array_equal(
         expected_counts.values,
@@ -248,4 +248,4 @@ def test_merfish_pipeline_cropped_data():
 
     corrcoef = np.corrcoef(tmp[:, 1], tmp[:, 0])[0, 1]
 
-    assert np.round(corrcoef, 4) == 0.9144
+    assert np.round(corrcoef, 4) == 0.9647

starfish/util/dtype.py
@@ -31,8 +31,9 @@ def preserve_float_range(
     else:
         data = array
 
-    if np.any(data < 0):
-        data[data < 0] = 0
+    negative = data < 0
+    if np.any(negative):
+        data[negative] = 0
     if np.any(data > 1):
         if rescale:
             data /= data.max()
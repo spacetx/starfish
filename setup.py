11cb51bc5e9f5d03ea46a99e0109a7bcd7fabd99
jmoore <josh@glencoesoftware.com>
2019-03-03 11:00:13 +0100

    Revert pbr

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import os
+import setuptools
 
-setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    pbr=True,
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Natural Language :: English",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+install_requires = [
+    line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "REQUIREMENTS.txt"))
+]
+
+setuptools.setup(
+    name="starfish",
+    version="0.0.33",
+    description="Pipelines and pipeline components for the analysis of image-based transcriptomics data",
+    author="Deep Ganguli",
+    author_email="dganguli@chanzuckerberg.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'napari': ['napari-gui==0.0.5.1'],
+    },
+    entry_points={
+        'console_scripts': [
+            "starfish=starfish.starfish:starfish",
+        ]
+    },
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
 )

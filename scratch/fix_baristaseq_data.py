4a985f9434347d2adda697a26ae833d8826b6380
Ambrose J. Carr <mail@ambrosejcarr.com>
2019-03-15 15:11:13 -0400

    fix script for baristaseq

--- scratch/fix_baristaseq_data.py
+++ scratch/fix_baristaseq_data.py
@@ -1,51 +1,36 @@
 # what follows is the history that was needed to fix the primaryFOV_000.json file
+
 import json
-dat = json.load('primaryFOV_000.json')
-with open('primaryFOV_000.json') as f:
-    dat = json.load(f)
-with open('primaryFOV_000.json') as f:
-    dat = f.read(150)
-dat
-pprint(dat)
-pprint(dat)
-from pprint import pprint
-pprint(dat)
-with open('primaryFOV_000.json') as f:
-    dat = json.load(f)
 import re
+import sys
 
-def clean_json(string):
-    string = re.sub(",[ \t\r\n]+}", "}", string)
-    string = re.sub(",[ \t\r\n]+\]", "]", string)
 
-    return string
-with open('primaryFOV_000.json') as f:
-    dat = f.read()
-cleaned = clean_json(dat)
-json = json.loads(cleaned)
-jon
-jon
-json
-json_dat = json
-import json
-json.dump(json_dat, "primary_corrected_FOV_000.json")
-with open("primary_corrected_FOV_000.json") as f:
-    json.dump(json_dat, f)
-with open("primary_corrected_FOV_000.json", 'w') as f:
-    json.dump(json_dat, f)
-json_dat
-json_dat.keys()
-json_dat['tiles'].keys()
-json_dat['tiles'][0].keys()
-for t in json_dat['tiles']:
-    for i in ('xyz'):
-        t['coordinates'][f'{i}c'] = t['coordinates'][f'{i}']
-        del t['coordinates'][f'{i}']
-json_dat
-with open("primary_corrected_FOV_000.json", 'w') as f:
-    json.dump(json_dat, f)
-for t in json_dat['tiles']:
-    for i in 'yx':
-        del t['indices'][f'{i}']
-with open("primary_corrected_FOV_000.json", 'w') as f:
-    json.dump(json_dat, f)
+def main(input_json: str, output_json: str) -> None:
+
+    def clean_json(string):
+        string = re.sub(",[ \t\r\n]+}", "}", string)
+        string = re.sub(",[ \t\r\n]+\]", "]", string)
+
+        return string
+
+    # read in the json file
+    with open(input_json) as f:
+        dat = f.read()
+        cleaned = clean_json(dat)
+        json_dat = json.loads(cleaned)
+
+    # fix some broken stuff
+    for t in json_dat['tiles']:
+        for i in ('xyz'):
+            t['coordinates'][f'{i}c'] = t['coordinates'][f'{i}']
+            del t['coordinates'][f'{i}']
+
+        for i in 'yx':
+            del t['indices'][f'{i}']
+
+    with open(output_json, 'w') as f:
+        json.dump(json_dat, f)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])

7d3e69f6d273267aa8382f50cfe2b0ef58400031
Ambrose J. Carr <mail@ambrosejcarr.com>
2019-03-22 18:34:49 -0400

    upload seqFISH notebook testing LocalSearchBlobDetector and PixelSpotDetector. Neither appear to work. Note that seqFISH requests SymmetricLocalSearchBlobDetector, which is not yet implemented. This is ostensibly to act as a positive control, however, so it's unclear if it would help in this instance.

--- notebooks/py/SeqFISH.py
+++ notebooks/py/SeqFISH.py
@@ -3,24 +3,197 @@
 #
 # EPY: stripped_notebook: {"metadata": {"kernelspec": {"display_name": "starfish", "language": "python", "name": "starfish"}, "language_info": {"codemirror_mode": {"name": "ipython", "version": 3}, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5"}}, "nbformat": 4, "nbformat_minor": 2}
 
+# EPY: START markdown
+## Starfish BaristaSeq Processing Example
+# EPY: END markdown
+
 # EPY: START code
+# EPY: ESCAPE %gui qt
+
+import os
+from copy import deepcopy
+from itertools import product
+
+import numpy as np
+import pandas as pd
+import skimage.filters
+import skimage.morphology
+from skimage.transform import SimilarityTransform, warp
+from tqdm import tqdm
+
+import starfish
 import starfish.data
+from starfish.spots import SpotFinder
+from starfish.types import Axes
 # EPY: END code
 
 # EPY: START markdown
-#This notebook demonstrates how to load SeqFISH data into starfish. Below loads a fraction of fov_000, a single field of view from the Intron-SeqFISH paper. We load the test data as the full dataset is ~ 5GB and may be difficult for some laptops to load.
+#Select data for a single field of view. 
 # EPY: END markdown
 
 # EPY: START code
-experiment = starfish.data.SeqFISH(use_test_data=True)
-fov = experiment['fov_000']
-stack = fov.get_image('primary')
+exp = starfish.data.SeqFISH(use_test_data=True)
 # EPY: END code
 
 # EPY: START code
-stack.shape
+img = exp['fov_000'].get_image('primary')
 # EPY: END code
 
 # EPY: START markdown
-#A processing pipeline is in the works. For now, you can explore the data in starfish.
+#The first step in BaristaSeq is to do some rough registration. For this data, the rough registration has been done for us by the authors, so it is omitted from this notebook.
 # EPY: END markdown
+
+# EPY: START markdown
+### Remove image background
+# EPY: END markdown
+
+# EPY: START markdown
+#To remove image background, BaristaSeq uses a White Tophat filter, which measures the background with a rolling disk morphological element and subtracts it from the image. 
+# EPY: END markdown
+
+# EPY: START code
+from skimage.morphology import opening, dilation, disk
+from functools import partial
+# EPY: END code
+
+# EPY: START markdown
+#If desired, the background that is being subtracted can be visualized
+# EPY: END markdown
+
+# EPY: START code
+# opening = partial(opening, selem=disk(3))
+
+# background = img.apply(
+#     opening,
+#     group_by={Axes.ROUND, Axes.CH, Axes.ZPLANE}, verbose=False, in_place=False
+# )
+
+# starfish.display(background)
+# EPY: END code
+
+# EPY: START code
+wth = starfish.image.Filter.WhiteTophat(masking_radius=3)
+background_corrected = wth.run(img, in_place=False)
+starfish.display(background_corrected)
+# EPY: END code
+
+# EPY: START markdown
+### Scale images to equalize spot intensities across channels
+#
+#The number of peaks are not uniform across rounds and channels, which prevents histogram matching across channels. Instead, a percentile value is identified and set as the maximum across channels, and the dynamic range is extended to equalize the channel intensities
+# EPY: END markdown
+
+# EPY: START code
+def scale_by_percentile(data, p=99.9):
+    data = np.asarray(data)
+    cval = np.percentile(data, p)
+    data = data / cval
+    data[data > 1] = 1
+    return data
+
+scaled = background_corrected.apply(
+    scale_by_percentile,
+    group_by={Axes.ROUND, Axes.CH}, verbose=False, in_place=False
+)
+# EPY: END code
+
+# EPY: START code
+starfish.display(scaled)
+# EPY: END code
+
+# EPY: START markdown
+### Remove residual background
+#
+#The background is fairly uniformly present below intensity=0.5. However, starfish's clip method currently only supports percentiles. To solve this problem, the intensities can be directly edited in the underlying numpy array. 
+# EPY: END markdown
+
+# EPY: START code
+from copy import deepcopy
+clipped = deepcopy(scaled)
+clipped.xarray.values[clipped.xarray.values < 0.7] = 0
+# EPY: END code
+
+# EPY: START code
+starfish.display(clipped)
+# EPY: END code
+
+# EPY: START markdown
+### Detect Spots
+#
+#Detect spots with a local search blob detector that identifies spots in all rounds and channels and matches them using a local search method. The local search starts in an anchor channel (default ch=1) and identifies the nearest spot in all subsequent imaging rounds. 
+# EPY: END markdown
+
+# EPY: START code
+threshold = 0.5
+
+lsbd = starfish.spots._detector.local_search_blob_detector.LocalSearchBlobDetector(
+    min_sigma=(1.5, 1.5, 1.5),
+    max_sigma=(8, 8, 8),
+    num_sigma=10,
+    threshold=threshold,
+    search_radius=7
+)
+intensities = lsbd.run(clipped)
+decoded = exp.codebook.decode_per_round_max(intensities.fillna(0))
+# EPY: END code
+
+# EPY: START code
+starfish.display(clipped, intensities)
+# EPY: END code
+
+# EPY: START markdown
+#Based on visual inspection, it looks like the spot correspondence across rounds isn't being detected well. Try the PixelSpotDecoder.
+# EPY: END markdown
+
+# EPY: START code
+psd = starfish.spots.PixelSpotDecoder.PixelSpotDecoder(
+    codebook=exp.codebook, metric='euclidean', distance_threshold=0.5, 
+    magnitude_threshold=0.1, min_area=7, max_area=50
+)
+pixel_decoded, ccdr = psd.run(clipped)
+# EPY: END code
+
+# EPY: START code
+ccdr.label_image.shape
+# EPY: END code
+
+# EPY: START code
+# look at the label image in napari
+label_image = starfish.ImageStack.from_numpy_array(np.reshape(ccdr.label_image, (1, 1, 29, 280, 280)))
+starfish.display(label_image)
+# EPY: END code
+
+# EPY: START code
+np.unique(ccdr.label_image[19])
+# EPY: END code
+
+# EPY: START markdown
+#Well, this definitely does not work. It's decoding pixels in subsequent rounds as new unique gene types...
+# EPY: END markdown
+
+# EPY: START markdown
+#Compare the number of spots being detected by the two spot finders
+# EPY: END markdown
+
+# EPY: START code
+print("pixel_decoder spots detected", int(np.sum(pixel_decoded['target'] != 'nan')))
+print("local search spot detector spots detected", int(np.sum(decoded['target'] != 'nan')))
+# EPY: END code
+
+# EPY: START markdown
+#Report the correlation between the two methods
+# EPY: END markdown
+
+# EPY: START code
+from scipy.stats import pearsonr
+
+# get the total counts for each gene from each spot detector
+pixel_decoded_gene_counts = pd.Series(*np.unique(pixel_decoded['target'], return_counts=True)[::-1])
+decoded_gene_counts = pd.Series(*np.unique(decoded['target'], return_counts=True)[::-1])
+
+# get the genes that are detected by both spot finders
+codetected = pixel_decoded_gene_counts.index.intersection(decoded_gene_counts.index).drop('nan')
+
+# report the correlation
+pearsonr(pixel_decoded_gene_counts[codetected], decoded_gene_counts[codetected])
+# EPY: END code

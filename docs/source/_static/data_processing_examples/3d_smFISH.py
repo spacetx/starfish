e17007600e7a100ab4177854230239953a1c2e1e
Ambrose J. Carr <mail@ambrosejcarr.com>
2019-04-01 09:49:01 -0400

    Add sphinx-gallery for data processing examples * uses 3d smfish notebook as test case * no changes to logic of testing (the new script is untested!)

--- docs/source/_static/data_processing_examples/3d_smFISH.py
+++ docs/source/_static/data_processing_examples/3d_smFISH.py
@@ -1,43 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
-#
-# EPY: stripped_notebook: {"metadata": {"kernelspec": {"display_name": "starfish", "language": "python", "name": "starfish"}, "language_info": {"codemirror_mode": {"name": "ipython", "version": 3}, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5"}}, "nbformat": 4, "nbformat_minor": 2}
+"""
+Single Field of View for sequential single-molecule FISH processed in 3d
+========================================================================
 
-# EPY: START markdown
-## Reproduce 3d smFISH results with Starfish
-#
-#This notebook walks through a work flow that analyzes one field of view of a mouse gene panel from the Allen Institute for Cell Science, using the starfish package.
-# EPY: END markdown
+This notebook walks through a work flow that analyzes one field of view of a mouse gene panel from
+the Allen Institute for Cell Science, using the starfish package.
+"""
 
-# EPY: START markdown
-#The 3d smFISH workflow run by the Allen runs a bandpass filter to remove high and low frequency signal and blurs over z with a 1-pixel gaussian to smooth the signal over the z-axis. low-intensity signal is (stringently) clipped from the images before and after these filters.
-#
-#Spots are then detected using a spot finder based on trackpy's locate method, which identifies local intensity maxima, and spots are matched to the gene they represent by looking them up in a codebook that records which (round, channel) matches which gene target.
-# EPY: END markdown
-
-# EPY: START markdown
-### Load imports
-# EPY: END markdown
-
-# EPY: START code
-# EPY: ESCAPE %gui qt5
-
-import os
 from typing import Optional, Tuple
-
-# import napari_gui
-import numpy as np
+from IPython import get_ipython
 
 import starfish
-from starfish import data, FieldOfView, IntensityTable
+import starfish.data
+from starfish import FieldOfView, IntensityTable
 
-# EPY: END code
+# equivalent to %gui qt
+ipython = get_ipython()
+ipython.magic("gui qt5")
 
-# EPY: START markdown
-### Initialize Pipeline Components with pre-selected parameters
-# EPY: END markdown
 
-# EPY: START code
+###################################################################################################
+# Define image filters
+# --------------------
+# The 3d smFISH workflow run by the Allen runs a bandpass filter to remove high and low frequency
+# signal and blurs over z with a 1-pixel gaussian to smooth the signal over the z-axis.
+#
+# low-intensity signal is (stringently) clipped from the images before and after these filters.
+
 # bandpass filter to remove cellular background and camera noise
 bandpass = starfish.image.Filter.Bandpass(lshort=.5, llong=7, threshold=0.0)
 
@@ -53,9 +41,15 @@ clip1 = starfish.image.Filter.Clip(p_min=50, p_max=100)
 # post-filter clip to eliminate all but the highest-intensity peaks
 clip2 = starfish.image.Filter.Clip(p_min=99, p_max=100, is_volume=True)
 
-# peak caller
+###################################################################################################
+# Define a spot detection method
+# ------------------------------
+# Spots are detected using a spot finder based on trackpy's locate method, which identifies
+# local intensity maxima, and spots are matched to the gene they represent by looking them up in a
+# codebook that records which (round, channel) matches which gene target.
+
 tlmpf = starfish.spots.SpotFinder.TrackpyLocalMaxPeakFinder(
-    spot_diameter=5, # must be odd integer
+    spot_diameter=5,  # must be odd integer
     min_mass=0.02,
     max_size=2,  # this is max radius
     separation=7,
@@ -65,17 +59,11 @@ tlmpf = starfish.spots.SpotFinder.TrackpyLocalMaxPeakFinder(
     verbose=True,
     is_volume=True,
 )
-# EPY: END code
 
-# EPY: START markdown
-### Combine pipeline components into a pipeline
-# EPY: END markdown
+###################################################################################################
+# Construct the pipeline
+# ----------------------
 
-# EPY: START markdown
-#Define a function that identifies spots of a field of view.
-# EPY: END markdown
-
-# EPY: START code
 def processing_pipeline(
     experiment: starfish.Experiment,
     fov_name: str,
@@ -101,7 +89,9 @@ def processing_pipeline(
     primary_image = experiment[fov_name].get_image(FieldOfView.PRIMARY_IMAGES)
     all_intensities = list()
     codebook = experiment.codebook
+
     for primary_image in experiment[fov_name].iterate_image_type(FieldOfView.PRIMARY_IMAGES):
+
         print("Filtering images...")
         filter_kwargs = dict(
             in_place=True,
@@ -118,27 +108,20 @@ def processing_pipeline(
         all_intensities.append(spot_attributes)
 
     spot_attributes = IntensityTable.concatanate_intensity_tables(all_intensities)
+
     print("Decoding spots...")
     decoded = codebook.decode_per_round_max(spot_attributes)
-    decoded = decoded[decoded["total_intensity"]>.025]
+    decoded = decoded[decoded["total_intensity"] > .025]
 
     return primary_image, decoded
-# EPY: END code
 
-# EPY: START markdown
-### Run the pipeline on a field of view
-# EPY: END markdown
+###################################################################################################
+# Load data, run pipeline, display results
+# ----------------------------------------
 
-# EPY: START code
 experiment = starfish.data.allen_smFISH(use_test_data=True)
 
 image, intensities = processing_pipeline(experiment, fov_name='fov_001')
-# EPY: END code
 
-# EPY: START markdown
-### Display the results
-# EPY: END markdown
-
-# EPY: START code
-viewer = starfish.display(image, intensities)
-# EPY: END code
+# uncomment the below line to visualize the output with the spot calls.
+# viewer = starfish.display(image, intensities)

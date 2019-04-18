174efbaa987c7841eca37dc5d5b84b0392a43587
Tony Tung <ttung@chanzuckerberg.com>
2019-04-17 20:41:36 -0700

    Alternate approach of hiding stuff
    Doing it in a web editor.  What could go wrong?

--- starfish/__init__.py
+++ starfish/__init__.py
@@ -1,11 +1,9 @@
 # deal with numpy import warnings due to cython
 # See: https://stackoverflow.com/questions/40845304/
 #      runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility)
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")  # noqa
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")  # noqa
-
-import pkg_resources
+import warnings as _warnings
+_warnings.filterwarnings("ignore", message="numpy.dtype size changed")  # noqa
+_warnings.filterwarnings("ignore", message="numpy.ufunc size changed")  # noqa
 
 # image processing methods and objects
 from . import image
@@ -14,7 +12,7 @@ from . import spots
 # display images and spots
 from ._display import display
 # generated version number and commit hash
-from ._version import get_versions
+from ._version import _get_versions
 # top-level objects
 from .codebook.codebook import Codebook
 from .experiment.experiment import Experiment, FieldOfView
@@ -24,13 +22,10 @@ from .starfish import starfish
 
 
 # NOTE: if we move to python 3.7, we can produce this value at call time via __getattr__
-__version__ = get_versions()['version']
+__version__ = _get_versions()['version']
 __is_release_tag__ = None
 if "+" not in str(__version__):
     __is_release_tag__ = f"Release: {__version__}"
-del get_versions
 
 if __name__ == "__main__":
     starfish()
-
-del warnings, pkg_resources

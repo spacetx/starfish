a18f21d96062635797a61c1763b5dbacd94d85e3
Ambrose J Carr <ambrosejcarr@users.noreply.github.com>
2019-04-23 10:23:16 -0700

    Render notebooks (#1250)
    * check in mostly-run notebooks
    
    * add rendered starmap

--- notebooks/py/BaristaSeq.py
+++ notebooks/py/BaristaSeq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 #
-# EPY: stripped_notebook: {"metadata": {"kernelspec": {"display_name": "starfish-venv", "language": "python", "name": "starfish-venv"}, "language_info": {"codemirror_mode": {"name": "ipython", "version": 3}, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5"}}, "nbformat": 4, "nbformat_minor": 2}
+# EPY: stripped_notebook: {"metadata": {"kernelspec": {"display_name": "starfish", "language": "python", "name": "starfish"}, "language_info": {"codemirror_mode": {"name": "ipython", "version": 3}, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5"}}, "nbformat": 4, "nbformat_minor": 2}
 
 # EPY: START code
 # EPY: ESCAPE %matplotlib inline

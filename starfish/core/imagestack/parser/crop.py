c19d4b7bf96dbf25424072a2901ec53eb93b29eb
shanaxel42 <shannon.axelrod@chanzuckerberg.com>
2019-05-13 14:23:30 -0700

    Adding back ability to crop on fov.get_image() (#1329)
    * adding back ability to crop on fov.get_image

--- starfish/core/imagestack/parser/crop.py
+++ starfish/core/imagestack/parser/crop.py
@@ -58,6 +58,31 @@ class CropParameters:
         if axis_type == Axes.ZPLANE and self._permitted_zplanes:
             self._permitted_zplanes.add(permitted_axis)
 
+    def further_crop(self, further_crop_params) -> "CropParameters":
+        if (further_crop_params._permitted_chs
+                and not further_crop_params._permitted_chs.issubset(self._permitted_chs)):
+            raise ValueError("Can only provide further ch crop params within the existing set of"
+                             "ch crop params")
+        else:
+            self._permitted_chs = further_crop_params._permitted_chs
+
+        if (further_crop_params._permitted_rounds
+                and not further_crop_params._permitted_rounds.issubset(self._permitted_rounds)):
+            raise ValueError("Can only provide further round crop params within the existing set of"
+                             "round crop params")
+        else:
+            self._permitted_rounds = further_crop_params._permitted_rounds
+
+        if (further_crop_params._permitted_zplanes
+                and not further_crop_params._permitted_zplanes.issubset(self._permitted_zplanes)):
+            raise ValueError("Can only provide further zplane crop params within the existing set "
+                             "of zplane crop params")
+        else:
+            self._permitted_zplanes = further_crop_params._permitted_zplanes
+        self._x_slice = further_crop_params._x_slice
+        self._y_slice = further_crop_params._y_slice
+        return self
+
     def filter_tilekeys(self, tilekeys: Collection[TileKey]) -> Collection[TileKey]:
         """
         Filters tilekeys for those that should be included in the resulting ImageStack.

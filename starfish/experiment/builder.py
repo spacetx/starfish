2005a3afb77fefcdf3bbb7a2d211d029a17cc7b3
Nicholas Mei <nicholas.mei@alleninstitute.org>
2019-05-23 16:48:35 -0700

    Add inplace TileFetcher module back to public builder API
    This commit addresses an import error encountered
    when trying to import inplace TileFetcher functionality.
    
    Example failing import that will be fixed by this commit:
    
    from starfish.experiment.builder.inplace import (
        InplaceFetchedTile,
        enable_inplace_mode,
        inplace_tile_opener)

--- starfish/experiment/builder.py
+++ starfish/experiment/builder.py
@@ -1,2 +1,6 @@
-from starfish.core.experiment.builder import build_image, write_experiment_json  # noqa: F401
+from starfish.core.experiment.builder import (   # noqa: F401
+    build_image,
+    inplace,
+    write_experiment_json,
+)
 from starfish.core.experiment.builder.providers import FetchedTile, TileFetcher  # noqa: F401

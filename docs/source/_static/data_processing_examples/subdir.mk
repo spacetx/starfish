61d0375ddbdee4496c6ebb587f06a570fb079687
Tony Tung <ttung@chanzuckerberg.com>
2019-06-03 11:58:17 -0700

    Fix how we structure the run notebook rules. (#1384)
    With this change, one can just run `make notebooks/py/smFISH.py` or `make smFISH.py`.
    
    For the examples in `docs/source/_static/data_processing_examples/`, we can also directly invoke them, i.e., `make 3d_smFISH.py`.
    
    Also, we run all notebooks with ipython instead of python now.

--- docs/source/_static/data_processing_examples/subdir.mk
+++ docs/source/_static/data_processing_examples/subdir.mk
@@ -1,15 +1,15 @@
 path := docs/source/_static/data_processing_examples
 py_files := $(wildcard $(path)/*.py)
 sh_files := $(wildcard $(path)/*.sh)
-py_run_targets := $(patsubst $(path)/%,run__%,$(py_files))
-sh_run_targets := $(patsubst $(path)/%,run__%,$(sh_files))
+py_run_targets := $(patsubst $(path)/%,%,$(py_files))
+sh_run_targets := $(patsubst $(path)/%,%,$(sh_files))
 
 PYTHON := ipython
 
 run-examples: $(py_run_targets) $(sh_run_targets)
 
-$(py_run_targets): run__% :
+$(py_run_targets): % :
 	[ -e $(path)/$*.skip ] || $(PYTHON) $(path)/$*
 
-$(sh_run_targets): run__% :
+$(sh_run_targets): % :
 	[ -e $(path)/$*.skip ] || $(SHELL) $(path)/$*

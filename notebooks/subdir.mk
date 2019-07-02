spacetx/starfish

commit 61d0375ddbdee4496c6ebb587f06a570fb079687

Fix how we structure the run notebook rules. (#1384)
With this change, one can just run `make notebooks/py/smFISH.py` or `make smFISH.py`.

For the examples in `docs/source/_static/data_processing_examples/`, we can also directly invoke them, i.e., `make 3d_smFISH.py`.

Also, we run all notebooks with ipython instead of python now.

notebooks/subdir.mk
@@ -1,11 +1,12 @@
-py_files := $(wildcard notebooks/py/*.py)
+pypath := notebooks/py
+py_files := $(wildcard $(pypath)/*.py)
 ipynb_files := $(wildcard notebooks/*.ipynb)
-py_run_targets := $(addprefix run__, $(py_files))
+py_run_targets := $(patsubst $(pypath)/%,%,$(py_files))
 py_check_targets := $(addprefix check__, $(py_files))
 ipynb_validate_targets := $(addprefix validate__, $(ipynb_files))
 ipynb_regenerate_targets := $(addprefix regenerate__notebooks/, $(addsuffix .ipynb, $(notdir $(basename $(py_files)))))
 py_regenerate_targets := $(addprefix regenerate__notebooks/py/, $(addsuffix .py, $(notdir $(basename $(ipynb_files)))))
-PYTHON := python
+PYTHON := ipython
 
 fast: $(ipynb_validate_targets)
 run-notebooks: $(py_run_targets)
@@ -14,7 +15,10 @@ validate-notebooks: $(ipynb_validate_targets)
 regenerate-ipynb: $(ipynb_regenerate_targets)
 regenerate-py: $(py_regenerate_targets)
 
-$(py_run_targets): run__%.py :
+$(py_run_targets): %.py :
+	[ -e $*.py.skip ] || $(PYTHON) $(pypath)/$*.py
+
+$(py_files): %.py :
 	[ -e $*.py.skip ] || $(PYTHON) $*.py
 
 $(py_check_targets): check__%.py :
@@ -30,3 +34,5 @@ $(ipynb_regenerate_targets): regenerate__notebooks/%.ipynb : notebooks/py/%.py
 
 $(py_regenerate_targets): regenerate__notebooks/py/%.py : notebooks/%.ipynb
 	nbencdec encode notebooks/$*.ipynb notebooks/py/$*.py
+
+.PHONY : $(py_files)

docs/source/_static/data_processing_examples/subdir.mk
@@ -1,15 +1,15 @@
 path := docs/source/_static/data_processing_examples
 py_files := $(wildcard $(path)/*.py)
 sh_files := $(wildcard $(path)/*.sh)
-py_run_targets := $(patsubst $(path)/%,run__%,$(py_files))
-sh_run_targets := $(patsubst $(path)/%,run__%,$(sh_files))
+py_run_targets := $(patsubst $(path)/%,%,$(py_files))
+sh_run_targets := $(patsubst $(path)/%,%,$(sh_files))
 
 PYTHON := ipython
 
 run-examples: $(py_run_targets) $(sh_run_targets)
 
-$(py_run_targets): run__% :
+$(py_run_targets): % :
 	[ -e $(path)/$*.skip ] || $(PYTHON) $(path)/$*
 
-$(sh_run_targets): run__% :
+$(sh_run_targets): % :
 	[ -e $(path)/$*.skip ] || $(SHELL) $(path)/$*

.travis.yml
@@ -29,40 +29,40 @@ jobs:
       script: make docker
     - name: smFISH Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: travis_wait make install-dev run__notebooks/py/smFISH.py
+      script: travis_wait make install-dev notebooks/py/smFISH.py
     - name: MERFISH Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: travis_wait make install-dev run__notebooks/py/MERFISH.py
+      script: travis_wait make install-dev notebooks/py/MERFISH.py
     - name: osmFISH Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/osmFISH.py
+      script: make install-dev notebooks/py/osmFISH.py
     - name: DARTFISH Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/DARTFISH.py
+      script: make install-dev notebooks/py/DARTFISH.py
     - name: Simulation Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/Starfish_simulation.py
+      script: make install-dev notebooks/py/Starfish_simulation.py
     - name: ISS Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/ISS.py
+      script: make install-dev notebooks/py/ISS.py
     - name: BaristaSeq Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/BaristaSeq.py
+      script: make install-dev notebooks/py/BaristaSeq.py
     - name: SeqFISH Notebook
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__notebooks/py/SeqFISH.py
+      script: make install-dev notebooks/py/SeqFISH.py
 #    - name: STARmap Notebook
 #      if: type = push and branch =~ /^(master|merge)/
-#      script: make install-dev run__notebooks/py/STARmap.py
+#      script: make install-dev notebooks/py/STARmap.py
     - name: 3D smFISH data processing example
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__3d_smFISH.py
+      script: make install-dev 3d_smFISH.py
     - name: iss_cli.sh data processing example
       if: type = push and branch =~ /^(master|merge)/
-      script: make install-dev run__iss_cli.sh
+      script: make install-dev iss_cli.sh
     - name: iss_pipeline.py data processing example
       if: type = push and branch =~ /^(master|merge)/
-      script: make TESTING=1 install-dev run__iss_pipeline.py
+      script: make TESTING=1 install-dev iss_pipeline.py
     - name: Released Notebooks
       if: type = cron and branch =~ /^(master|merge)/
       script: make check-notebooks install-released-notebooks-support run-notebooks

commit e1cb27532de5323adf83fe7e4159f149502ead79

Fix the path where we find skip files
This should unbreak the cronjob.  I think I broke this in #1384.  Whoops!

notebooks/subdir.mk
@@ -16,7 +16,7 @@ regenerate-ipynb: $(ipynb_regenerate_targets)
 regenerate-py: $(py_regenerate_targets)
 
 $(py_run_targets): %.py :
-	[ -e $*.py.skip ] || $(PYTHON) $(pypath)/$*.py
+	[ -e $(pypath)/$*.py.skip ] || $(PYTHON) $(pypath)/$*.py
 
 $(py_files): %.py :
 	[ -e $*.py.skip ] || $(PYTHON) $*.py
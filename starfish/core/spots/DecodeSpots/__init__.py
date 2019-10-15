83059fcb20aa093d15b9b3aa67b6f4dbed73dc86
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2019-10-15 08:25:39 -0700

    adding MetricDecoder to DecodeSpots (#1596)

--- starfish/core/spots/DecodeSpots/__init__.py
+++ starfish/core/spots/DecodeSpots/__init__.py
@@ -1,4 +1,5 @@
 from ._base import DecodeSpotsAlgorithm
+from .metric_decoder import MetricDistance
 from .per_round_max_channel_decoder import PerRoundMaxChannel
 
 # autodoc's automodule directive only captures the modules explicitly listed in __all__.

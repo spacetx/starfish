16232052977b9b1738acd8016bd43d1b968f6d98
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2019-10-15 09:24:20 -0700

    5.) refactoring localMaxPeakFinder/osmFIsh (#1594)
    * refactoring localMaxPeakFinder
    
    * adding MetricDecoder to DecodeSpots (#1596)

--- starfish/core/spots/FindSpots/__init__.py
+++ starfish/core/spots/FindSpots/__init__.py
@@ -1,5 +1,6 @@
 from ._base import FindSpotsAlgorithm
 from .blob import BlobDetector
+from .local_max_peak_finder import LocalMaxPeakFinder
 from .trackpy_local_max_peak_finder import TrackpyLocalMaxPeakFinder
 
 # autodoc's automodule directive only captures the modules explicitly listed in __all__.

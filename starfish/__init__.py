01b4b7de631f50d4987088b2c24c9d2a90a29bab
Tony Tung <ttung@chanzuckerberg.com>
2019-11-13 08:05:14 -0800

    RFC: Add a morphology package to hold BinaryMaskCollection, LabelImage, and their respective operators
    Methods that create BinaryMaskCollections and LabelImages do not fit neatly into buckets with generally identical inputs and outputs.  The goal is to bucket all the morphological transformations by what they produce, and toss them all in the morphological package for ease of discovery.
    
    An additional bonus of unifying binary_mask/util.py and label_image/util.py was realized as well here.
    
    Test plan: this is a codemod, so travis

--- starfish/__init__.py
+++ starfish/__init__.py
@@ -13,13 +13,13 @@ from .core import (
 # display images and spots
 from .core._display import display
 # top-level objects
-from .core.binary_mask import BinaryMaskCollection
 from .core.codebook.codebook import Codebook
 from .core.experiment.experiment import Experiment, FieldOfView
 from .core.expression_matrix.expression_matrix import ExpressionMatrix
 from .core.imagestack.imagestack import ImageStack
 from .core.intensity_table.decoded_intensity_table import DecodedIntensityTable
 from .core.intensity_table.intensity_table import IntensityTable
-from .core.label_image import LabelImage
+from .core.morphology.object.binary_mask import BinaryMaskCollection
+from .core.morphology.object.label_image import LabelImage
 from .core.segmentation_mask import SegmentationMaskCollection
 from .core.util.logging import Log
--- starfish/core/image/Segment/watershed.py
+++ starfish/core/image/Segment/watershed.py
@@ -7,10 +7,10 @@ from showit import image
 from skimage.feature import peak_local_max
 from skimage.morphology import watershed
 
-from starfish.core.binary_mask import BinaryMaskCollection
 from starfish.core.image.Filter.util import bin_open, bin_thresh
 from starfish.core.imagestack.imagestack import ImageStack
-from starfish.core.label_image import LabelImage
+from starfish.core.morphology.object.binary_mask import BinaryMaskCollection
+from starfish.core.morphology.object.label_image import LabelImage
 from starfish.core.types import Axes, Coordinates, Number
 from ._base import SegmentAlgorithm
 

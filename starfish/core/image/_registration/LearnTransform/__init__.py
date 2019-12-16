86b6d8d4aca2211ed7819de33d0d272a8f471df6
Tony Tung <ttung@chanzuckerberg.com>
2019-12-16 11:00:47 -0800

    Fix definition of __all__
    @shanaxel42 pointed out that a lot of these are not filters.  Replaced them with a more generalized term that is conducive to copy-pasta. :)
    
    BOOM.  Also simplified the code.
    
    Test plan: make -j lint mypy

--- starfish/core/image/_registration/LearnTransform/__init__.py
+++ starfish/core/image/_registration/LearnTransform/__init__.py
@@ -2,9 +2,9 @@ from ._base import LearnTransformAlgorithm
 from .translation import Translation
 
 # autodoc's automodule directive only captures the modules explicitly listed in __all__.
-all_filters = {
-    filter_name: filter_cls
-    for filter_name, filter_cls in locals().items()
-    if isinstance(filter_cls, type) and issubclass(filter_cls, LearnTransformAlgorithm)
-}
-__all__ = list(all_filters.keys())
+__all__ = list(set(
+    implementation_name
+    for implementation_name, implementation_cls in locals().items()
+    if (isinstance(implementation_cls, type)
+        and issubclass(implementation_cls, LearnTransformAlgorithm))
+))
--- starfish/core/morphology/Binarize/__init__.py
+++ starfish/core/morphology/Binarize/__init__.py
@@ -3,9 +3,8 @@ from ._base import BinarizeAlgorithm
 from .threshold import ThresholdBinarize
 
 # autodoc's automodule directive only captures the modules explicitly listed in __all__.
-all_filters = {
-    filter_name: filter_cls
-    for filter_name, filter_cls in locals().items()
-    if isinstance(filter_cls, type) and issubclass(filter_cls, BinarizeAlgorithm)
-}
-__all__ = list(all_filters.keys())
+__all__ = list(set(
+    implementation_name
+    for implementation_name, implementation_cls in locals().items()
+    if isinstance(implementation_cls, type) and issubclass(implementation_cls, BinarizeAlgorithm)
+))

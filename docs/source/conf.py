de9d6d0d67b7a9975ba2a1e20d4e1561e94d38e9
Justin Kiggins <justin.kiggins@gmail.com>
2019-12-20 10:28:15 -0800

    bootstrap

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,17 +121,20 @@ sphinx_gallery_conf = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'bootstrap'
 
-# import sphinx_bootstrap_theme
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'bootstrap'
+
+import sphinx_bootstrap_theme
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'bootswatch_theme': "lumen",
+    'bootstrap_version': "3",
+}
 
 html_favicon = '_static/favicon.ico'
 

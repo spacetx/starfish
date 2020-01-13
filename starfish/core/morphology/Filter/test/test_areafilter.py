f56ae24ae1a3417affa00cdfbb5ceca4d9dc3514
Tony Tung <ttung@chanzuckerberg.com>
2020-01-13 14:07:44 -0800

    Use kwargs for area filter and validate args (#1718)
    Fixes #1706

--- starfish/core/morphology/Filter/test/test_areafilter.py
+++ starfish/core/morphology/Filter/test/test_areafilter.py
@@ -1,10 +1,12 @@
+import pytest
+
 from starfish.core.morphology.binary_mask.test.factories import binary_mask_collection_2d
 from ..areafilter import AreaFilter
 
 
 def test_empty_filter():
     input_mask_collection = binary_mask_collection_2d()
-    output_mask_collection = AreaFilter(None, None).run(input_mask_collection)
+    output_mask_collection = AreaFilter().run(input_mask_collection)
     assert len(output_mask_collection) == len(input_mask_collection)
 
     for mask_num in range(len(input_mask_collection)):
@@ -15,11 +17,16 @@ def test_empty_filter():
 
 def test_min_area():
     input_mask_collection = binary_mask_collection_2d()
-    output_mask_collection = AreaFilter(6, None).run(input_mask_collection)
+    output_mask_collection = AreaFilter(min_area=6).run(input_mask_collection)
     assert len(output_mask_collection) == 1
 
 
 def test_max_area():
     input_mask_collection = binary_mask_collection_2d()
-    output_mask_collection = AreaFilter(None, 5).run(input_mask_collection)
+    output_mask_collection = AreaFilter(max_area=5).run(input_mask_collection)
     assert len(output_mask_collection) == 1
+
+
+def test_illegal_areas():
+    with pytest.raises(ValueError):
+        AreaFilter(min_area=7, max_area=1)

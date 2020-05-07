7f15413798ad16bd8d251bacf17bb637cf52e1a6
mattcai <39178165+mattcai@users.noreply.github.com>
2020-05-06 17:51:13 -0700

    fix how spot_ids are handled by build_traces_sequential and Label._assign() (#1872)
    * Made build_traces_sequential reassign unique spot_ids to every feature after concatenating PerImageSliceResults. Also made Label._assign() explicitly label features by spot_id instead of row number, ensuring that labeling is accurate even when spot_ids do not match row_numbers in an IntensityTable. Spot_ids must still be unique for accurate labeling though.

--- starfish/core/spots/DecodeSpots/trace_builders.py
+++ starfish/core/spots/DecodeSpots/trace_builders.py
@@ -55,7 +55,10 @@ def build_traces_sequential(spot_results: SpotFindingResults, **kwargs) -> Inten
 
     """
 
-    all_spots = pd.concat([sa.spot_attrs.data for sa in spot_results.values()], sort=True)
+    all_spots = pd.concat([sa.spot_attrs.data for sa in spot_results.values()],
+                          ignore_index=True, sort=True)
+    # reassign spot_ids to index number so they are unique
+    all_spots['spot_id'] = all_spots.index
 
     intensity_table = IntensityTable.zeros(
         spot_attributes=SpotAttributes(all_spots),

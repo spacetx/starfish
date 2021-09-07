f485e6fcff59a5b39279f8483d969cfc4874e780
Nicholas Mei <nicholas.mei@outlook.com>
2021-09-06 21:40:02 -0700

    Add to_dict and from_dict methods to TransformsList class
    This commit adds the ability to output the TransformsList
    as a Python dictionary as well as the ability to
    instantiate a TransformsList from a Python dictionary.

--- starfish/core/image/_registration/transforms_list.py
+++ starfish/core/image/_registration/transforms_list.py
@@ -81,6 +81,34 @@ class TransformsList:
                 f"supports transform list formats from {MIN_SUPPORTED_VERSION} to "
                 f"{MAX_SUPPORTED_VERSION}")
 
+    def to_dict(self) -> dict:
+        """
+        Save the TransformsList as a Python dictionary.
+
+        Returns
+        -------
+        dict
+            A Python dictionary with the following keys and values:
+                {DocumentKeys.TRANSFORMS_LIST}: List[Tuple[str, str, List[List[float]]]]
+                    The value is a tuple of:
+                        selector_string: A string that specifies which starfish dimensions
+                                         to apply the transform to (e.g. "{r: 0}")
+                        transform_type: Specifies the transform type (e.g. "similarity")
+                        transform_matrix: A list of lists of floats
+                {DocumentKeys.VERSION_KEY}: str
+        """
+        transforms_array = []
+        # Need to convert Axes to str values, and TransformationObjects to array
+        for selectors, transform_type, transforms_object in self.transforms:
+            transforms_matrix = transforms_object.params.tolist()
+            selectors_str = {k.value: v for k, v in selectors.items()}
+            transforms_array.append((selectors_str, transform_type.value, transforms_matrix))
+        transforms_document = {
+            DocumentKeys.TRANSFORMS_LIST: transforms_array,
+            DocumentKeys.VERSION_KEY: str(CURRENT_VERSION)
+        }
+        return transforms_document
+
     def to_json(self, filename: str) -> None:
         """
         Saves the TransformsList to a json file.
@@ -89,18 +117,30 @@ class TransformsList:
         ----------
         filename : str
         """
-        transforms_array = []
-        # Need to convert Axes to str values, and TransformationObjects to array
-        for selectors, transform_type, transforms_object in self.transforms:
-            transforms_matrix = transforms_object.params.tolist()
-            selectors_str = {k.value: v for k, v in selectors.items()}
-            transforms_array.append((selectors_str, transform_type.value, transforms_matrix))
-            transforms_document = {
-                DocumentKeys.TRANSFORMS_LIST: transforms_array,
-                DocumentKeys.VERSION_KEY: str(CURRENT_VERSION)
-            }
+        transforms_document = self.to_dict()
         with open(filename, 'w') as f:
             json.dump(transforms_document, f)
+
+    @classmethod
+    def from_dict(cls, transforms_document: dict) -> "TransformsList":
+        """
+        Load a TransformsList from a Python dictionary.
+
+        Returns
+        -------
+        TransformsList
+        """
+        version_str = transforms_document[DocumentKeys.VERSION_KEY]
+        cls._verify_version(version_str)
+        transforms_array = transforms_document[DocumentKeys.TRANSFORMS_LIST]
+
+        transforms_list: List[Tuple[Mapping[Axes, int], TransformType, GeometricTransform]] = list()
+        for selectors_str, transform_type_str, transforms_matrix in transforms_array:
+            selectors = {Axes(k): v for k, v in selectors_str.items()}
+            transform_type = TransformType(transform_type_str)
+            transform_object = transformsTypeMapping[transform_type](np.array(transforms_matrix))
+            transforms_list.append((selectors, transform_type, transform_object))
+        return cls(transforms_list)
 
     @classmethod
     def from_json(cls, url_or_path: str) -> "TransformsList":
@@ -118,16 +158,7 @@ class TransformsList:
         TransformsList
         """
         config = StarfishConfig()
-        transforms_list: List[Tuple[Mapping[Axes, int], TransformType, GeometricTransform]] = list()
         backend, name, _ = resolve_path_or_url(url_or_path, backend_config=config.slicedimage)
         with backend.read_contextmanager(name) as fh:
             transforms_document = json.load(fh)
-            version_str = transforms_document[DocumentKeys.VERSION_KEY]
-            cls._verify_version(version_str)
-            transforms_array = transforms_document[DocumentKeys.TRANSFORMS_LIST]
-        for selectors_str, transform_type_str, transforms_matrix in transforms_array:
-            selectors = {Axes(k): v for k, v in selectors_str.items()}
-            transform_type = TransformType(transform_type_str)
-            transform_object = transformsTypeMapping[transform_type](np.array(transforms_matrix))
-            transforms_list.append((selectors, transform_type, transform_object))
-        return cls(transforms_list)
+        return cls.from_dict(transforms_document=transforms_document)
